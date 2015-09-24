4d6c140c3d3041ad95fe3c044c4b5352bb9b6ac3
Jonathan Buttigieg <jonathan.buttigieg@yahoo.fr>
2015-09-24 16:46:30 +0200

    Remove rocket_capacity mention & improve the display by checking the capacities

--- inc/common/admin-bar.php
+++ inc/common/admin-bar.php
@@ -9,27 +9,31 @@ defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
 add_action( 'admin_bar_menu', '_imagify_admin_bar', PHP_INT_MAX );
 function _imagify_admin_bar( $wp_admin_bar )
 {
-	if ( ! current_user_can( apply_filters( 'rocket_capacity', 'manage_options' ) ) )  {
+	if( ! current_user_can( 'upload_files' ) ) {
 		return;
 	}
-
+	
 	// Parent
     $wp_admin_bar->add_menu( array(
 	    'id'    => 'imagify',
 	    'title' => 'Imagify',
 	    'href'  => get_imagify_admin_url(),
 	));
-
-	// Settings
-	$wp_admin_bar->add_menu(array(
-		'parent' => 'imagify',
-		'id' 	 => 'imagify-settings',
-		'title'  => __( 'Settings', 'imagify' ),
-	    'href'   => get_imagify_admin_url(),
-	));
-
+	
+	$cap = ( imagify_is_active_for_network() ) ? 'manage_network_options' : 'manage_options';
+	/** This filter is documented in inc/admin/options.php */
+	if (  current_user_can( apply_filters( 'imagify_capacity', $cap ) ) )  {
+		// Settings
+		$wp_admin_bar->add_menu(array(
+			'parent' => 'imagify',
+			'id' 	 => 'imagify-settings',
+			'title'  => __( 'Settings', 'imagify' ),
+		    'href'   => get_imagify_admin_url(),
+		));	
+	}
+	
 	// Bulk Optimization
-	if ( imagify_valid_key() && ! is_network_admin() ) {
+	if ( imagify_valid_key() && ! is_network_admin() && current_user_can( 'upload_files' ) ) {
 		$wp_admin_bar->add_menu(array(
 			'parent' => 'imagify',
 			'id' 	 => 'imagify-bulk-optimization',
