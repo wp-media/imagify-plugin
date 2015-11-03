752e2f5e67acf4bd35ed1f908fc1a551e9fd27d3
Jonathan Buttigieg <jonathan.buttigieg@yahoo.fr>
2015-11-03 08:37:02 +0100

    Protect the JS value

--- inc/admin/enqueue.php
+++ inc/admin/enqueue.php
@@ -177,8 +177,8 @@ function _imagify_admin_print_intercom() {
 	<script>
 	window.intercomSettings = {
 		app_id: "cd6nxj3z",
-		user_id: <?php echo $user->id; ?>,
-		email: "<?php echo $user->email; ?>",
+		user_id: <?php echo (int) $user->id; ?>,
+		email: "<?php echo esc_js( $user->email ); ?>",
 	};
 	</script>
 	<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/cd6nxj3z';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
