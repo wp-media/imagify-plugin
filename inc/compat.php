72c55aaf2f420bfb4601b0c5f2f024a8ac3d99e4
Jonathan Buttigieg <jonathan.buttigieg@yahoo.fr>
2015-11-03 11:39:50 +0100

    Fix PHP Parse error: syntax error, unexpected ':' in ../inc/compat.php on line 17

--- inc/compat.php
+++ inc/compat.php
@@ -14,7 +14,7 @@ defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
 if ( ! function_exists( 'curl_file_create' ) ) {
     function curl_file_create( $filename, $mimetype = '', $postname = '' ) {
         return "@$filename;filename="
-            . ( $postname ?: basename( $filename ) )
+            . ( $postname ? $postname : basename( $filename ) )
             . ( $mimetype ? ";type=$mimetype" : '' );
     }
 }
