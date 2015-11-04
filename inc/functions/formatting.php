787a1f273a88c1275eadf11cc8eff8ee486a06ac
Jonathan Buttigieg <jonathan.buttigieg@yahoo.fr>
2015-11-04 11:36:31 +0100

    Fix PHP Notice: Undefined offset: 1 in ../inc/functions/formatting.php on line 16

--- inc/functions/formatting.php
+++ inc/functions/formatting.php
@@ -1,5 +1,6 @@
 <?php 
 defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
+
 /**
  * Round UP to nearest half integer
  *
@@ -10,13 +11,18 @@ defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
  * @return int $number  The formatted number
  */
 function imagify_round_half_five( $number ) {
-    $number = strval($number);
-    $number = explode('.', $number);
-    $decimal = floatval( '0.'.substr( $number[1], 0 , 2 ) ); // cut only 2 number
-    if( $decimal > 0 ) {
+    $number = strval( $number );
+    $number = explode( '.', $number );
+      
+    if ( (bool) ! $number[1] ) {
+	    return $number[0];
+    }
+    
+    $decimal = floatval( '0.' . substr( $number[1], 0 , 2 ) ); // cut only 2 number
+    if ( $decimal > 0 ) {
         if( $decimal <= 0.5 ) {
             return floatval( $number[0] ) + 0.5;
-        } elseif( $decimal > 0.5 && $decimal <= 0.99 ) {
+        } elseif ( $decimal > 0.5 && $decimal <= 0.99 ) {
             return floatval( $number[0]) + 1;
         }
     } else {
