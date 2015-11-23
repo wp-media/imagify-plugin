4a5ed86c497a561255971db29fe7e025152fb936
Jonathan Buttigieg <jonathan.buttigieg@yahoo.fr>
2015-11-23 18:58:05 +0100

    _e() instead of echo __()

--- inc/admin/ui/bulk.php
+++ inc/admin/ui/bulk.php
@@ -96,7 +96,7 @@ function _imagify_display_bulk_page() {
 					<?php if ( get_imagify_option( 'backup', 0 ) == "1" ) { ?>
 
 						<p>
-							<strong><?php _e( 'Select your compression level', 'imagify' ); ?></strong>
+							<strong><?php _e( 'Select Your Compression Level', 'imagify' ); ?></strong>
 							<br>
 							<?php 
 								$default_set = __( 'Ultra', 'imagify' );
@@ -109,7 +109,7 @@ function _imagify_display_bulk_page() {
 										break;
 								}
 
-								echo __( 'Your default setting:', 'imagify' ) . '&nbsp;<strong class="imagify-primary">' . $default_set . '</strong>';
+								_e( 'Your default setting:', 'imagify' ) . '&nbsp;<strong class="imagify-primary">' . $default_set . '</strong>';
 							?>
 						</p>
 
