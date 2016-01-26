473693f2c4a0ec923c25e0537efaa5cf81e53134
Geoffrey Crofte <support@creativejuiz.com>
2016-01-26 17:47:58 +0100

    WP Rocket sidebar offer
    Needs Jonathan for translation

--- inc/admin/ui/options.php
+++ inc/admin/ui/options.php
@@ -15,15 +15,28 @@ function _imagify_display_options_page() {
 			<div class="imagify-col imagify-sidebar">
 				<div class="imagify-sidebar-section">
 					<span class="imagify-sidebar-title">
-						<img src="<?php echo IMAGIFY_ASSETS_IMG_URL; ?>phone.svg" width="19" height="16" alt=""> <?php _e( 'Discover our other products', 'imagify' ); ?>
+						<img src="<?php echo IMAGIFY_ASSETS_IMG_URL; ?>phone.svg" width="19" height="16" alt=""> <?php _e( 'Is your WordPress website too slow?', 'imagify' ); ?>
 					</span>
 					<ul class="wp-media-products">
 						<li>
-							<a tabindex="1" class="wprocket-link" href="http://wp-rocket.me?utm_source=imagify-plugin">
-								<?php _e( 'Is your WordPress website too slow?', 'imagify' ); ?>
-								<br/>
-								<?php _e( 'Discover the best caching plugin to speed up your website.', 'imagify' ); ?>
-							</a>
+							<div class="links wprocket-link">
+								<p><strong>
+									<?php _e( 'Discover the best caching plugin to speed up your website.', 'imagify' ); ?>
+								</strong></p>
+
+								<p class="imagify-big-text">
+									<?php
+									$discount_percent = '20%';
+									$discount_code = 'IMAGIFY20';
+
+									printf( __( '%sGet %s off%s with this coupon code:%s', 'imagify' ), '<span class="imagify-mark-styled"><span>', '<strong>' . $discount_percent, '</strong></span></span>', '<span class="imagify-discount-code">' . $discount_code . '</span>' );
+									?>
+								</p>
+
+								<p>
+									<a class="btn btn-rocket"  href="http://wp-rocket.me/?utm_source=imagify&amp;utm_medium=plugin&amp;utm_campaign=coupon-code"><?php _e( 'Get WP Rocket now', 'imagify' ); ?></a>
+								</p>
+							</div>
 						</li>
 					</ul>
 				</div>
