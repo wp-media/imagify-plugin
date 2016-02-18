583633f45cfde42771260975766184c44e21e32a
Jonathan Buttigieg <jonathan.buttigieg@yahoo.fr>
2016-02-18 17:41:29 +0100

    Fix issue caused by YOAST!!!!!!!!!! Thanks Geoffrey for the fix

--- inc/admin/enqueue.php
+++ inc/admin/enqueue.php
@@ -256,3 +256,19 @@ function _imagify_admin_print_intercom() {
 	<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/cd6nxj3z';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
 <?php
 }
+
+/**
+ * Remove Yoast SEO bugged script
+ *
+ * @since 1.4.1
+ */
+add_action( 'wp_print_scripts', '_imagify_dequeue_yoastseo_script' );
+function _imagify_dequeue_yoastseo_script() {
+    global $pagenow;
+    $current_screen = get_current_screen();
+
+    if ( isset( $current_screen ) && 'post' === $current_screen->base && 'attachment' === $current_screen->post_type ) {
+        wp_dequeue_script( 'yoast-seo' );
+        wp_deregister_script( 'yoast-seo' );
+    }
+}
