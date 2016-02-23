b9b08fc85dd85b6a4be5a42b9fd559c9db6a2190
Jonathan Buttigieg <jonathan.buttigieg@yahoo.fr>
2016-02-23 15:19:22 +0100

    Limit at 10 minutes transients used for async optimization + remove old transients

--- inc/admin/ajax.php
+++ inc/admin/ajax.php
@@ -25,7 +25,7 @@ function _do_admin_post_imagify_manual_upload() {
 	
 	$attachment_id = $_GET['attachment_id'];
 	
-	set_transient( 'imagify-async-in-progress-' . $attachment_id, true );
+	set_transient( 'imagify-async-in-progress-' . $attachment_id, true, 10 * MINUTE_IN_SECONDS );
 	
 	$attachment = new Imagify_Attachment( $attachment_id );
 	
