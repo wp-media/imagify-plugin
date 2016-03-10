a16ce423ec055a65ecb5132690a07fbd51909dfa
Jonathan Buttigieg <jonathan.buttigieg@yahoo.fr>
2016-03-10 16:35:51 +0100

    Use filter instead of non dynamic value - poke @BoiteAWeb

--- inc/functions/admin-stats.php
+++ inc/functions/admin-stats.php
@@ -13,7 +13,7 @@ function imagify_count_attachments() {
 	$count = get_object_vars( $count );
 	$count = array_sum( $count );
 	
-	if ( $count > 10000 ) {
+	if ( $count > apply_filters( 'imagify_unoptimized_attachment_limit', 10000 ) ) {
 		set_transient( IMAGIFY_SLUG . '_large_library', 1 );	
 	}
 	
@@ -66,21 +66,29 @@ function imagify_count_exceeding_attachments() {
  * @return int The number of attachments.
  */
 function imagify_count_error_attachments() {
-	$query = new WP_Query(
-		array(
-			'post_type'              => 'attachment',
-			'post_status'			 => 'inherit',
-			'post_mime_type'         => get_imagify_mime_type(),
-			'meta_key'				 => '_imagify_status',
-			'meta_value'			 => 'error',
-			'posts_per_page'         => -1,
-			'update_post_term_cache' => false,
-			'no_found_rows'          => true,
-			'fields'                 => 'ids'
-		)
-	);
+	$transient = IMAGIFY_SLUG . '_count_error_attachments';
+	$count 	   = get_transient( $transient );
+	
+	if ( ! $count ) {
+		$query = new WP_Query(
+			array(
+				'post_type'              => 'attachment',
+				'post_status'			 => 'inherit',
+				'post_mime_type'         => get_imagify_mime_type(),
+				'meta_key'				 => '_imagify_status',
+				'meta_value'			 => 'error',
+				'posts_per_page'         => -1,
+				'update_post_term_cache' => false,
+				'no_found_rows'          => true,
+				'fields'                 => 'ids'
+			)
+		);
+		
+		$count = $query->post_count;
+		set_transient( $transient, $query->post_count );
+	}
 
-	return (int) $query->post_count;
+	return (int) $count;
 }
 
 /*
@@ -91,32 +99,40 @@ function imagify_count_error_attachments() {
  * @return int The number of attachments.
  */
 function imagify_count_optimized_attachments() {
-	$query = new WP_Query(
-		array(
-			'post_type'              => 'attachment',
-			'post_status'			 => 'inherit',
-			'post_mime_type'         => get_imagify_mime_type(),
-			'meta_query'      => array(
-			'relation'    => 'or',
-				array(
-					'key'     => '_imagify_status',
-					'value'   => 'success',
-					'compare' => '='
+	$transient = IMAGIFY_SLUG . '_count_optimized_attachments';
+	$count 	   = get_transient( $transient );
+	
+	if ( ! $count ) {
+		$query = new WP_Query(
+			array(
+				'post_type'              => 'attachment',
+				'post_status'			 => 'inherit',
+				'post_mime_type'         => get_imagify_mime_type(),
+				'meta_query'      => array(
+				'relation'    => 'or',
+					array(
+						'key'     => '_imagify_status',
+						'value'   => 'success',
+						'compare' => '='
+					),
+					array(
+						'key'     => '_imagify_status',
+						'value'   => 'already_optimized',
+						'compare' => '='
+					)
 				),
-				array(
-					'key'     => '_imagify_status',
-					'value'   => 'already_optimized',
-					'compare' => '='
-				)
-			),
-			'posts_per_page'         => -1,
-			'update_post_term_cache' => false,
-			'no_found_rows'          => true,
-			'fields'                 => 'ids'
-		)
-	);
-
-	return (int) $query->post_count;
+				'posts_per_page'         => -1,
+				'update_post_term_cache' => false,
+				'no_found_rows'          => true,
+				'fields'                 => 'ids'
+			)
+		);
+		
+		$count = $query->post_count;
+		set_transient( $transient, $query->post_count );
+	}
+	
+	return (int) $count;
 }
 
 /*
@@ -155,62 +171,68 @@ function imagify_percent_optimized_attachments() {
  * @return array An array containing the optimization data.
  */
 function imagify_count_saving_data( $key = '' ) {
-	global $wpdb;
-
-	$original_size  = 0;
-	$optimized_size = 0;
-	$query = new WP_Query(
-		array(
-			'post_type'              => 'attachment',
-			'post_status'			 => 'inherit',
-			'post_mime_type'         => get_imagify_mime_type(),
-			'meta_key'				 => '_imagify_status',
-			'meta_value'			 => 'success',
-			'posts_per_page'         => -1,
-			'update_post_term_cache' => false,
-			'no_found_rows'          => true,
-			'fields'                 => 'ids'
-		)
-	);
-	$attachments = (array) $query->posts;
-
-	foreach( $attachments as $attachment_id ) {
-		$attachment = new Imagify_Attachment( $attachment_id );
-
-		// Check if the attachment extension is allowed
-		if ( ! wp_attachment_is_image( $attachment_id ) ) {
-			continue;
-		}
-
-		$stats_data    = $attachment->get_stats_data();
-		$original_data = $attachment->get_size_data( 'full' );
-
-		// Incremente the original sizes
-		if ( $attachment->is_optimized() ) {
-			$original_size  += ( $original_data['original_size'] ) ? $original_data['original_size'] : 0;
-			$optimized_size += ( $original_data['optimized_size'] ) ? $original_data['optimized_size'] : 0;
-		}
-		
-		$metadata = wp_get_attachment_metadata( $attachment_id );
-		$sizes    = ( isset( $metadata['sizes'] ) ) ? (array) $metadata['sizes'] : array();
-
-		// Incremente the thumbnails sizes
-		foreach ( $sizes as $size_key => $size_data ) {
-			$size_data = $attachment->get_size_data( $size_key );
-			if ( ! empty( $size_data['success'] ) ) {
-				$original_size  += ( $size_data['original_size'] ) ? $size_data['original_size'] : 0;
-				$optimized_size += ( $size_data['optimized_size'] ) ? $size_data['optimized_size'] : 0;
+	$transient = IMAGIFY_SLUG . '_count_saving_data';
+	$data 	   = get_transient( $transient );
+	
+	if ( ! $data ) {
+		global $wpdb;
+	
+		$original_size  = 0;
+		$optimized_size = 0;
+		$query = new WP_Query(
+			array(
+				'post_type'              => 'attachment',
+				'post_status'			 => 'inherit',
+				'post_mime_type'         => get_imagify_mime_type(),
+				'meta_key'				 => '_imagify_status',
+				'meta_value'			 => 'success',
+				'posts_per_page'         => -1,
+				'update_post_term_cache' => false,
+				'no_found_rows'          => true,
+				'fields'                 => 'ids'
+			)
+		);
+		$attachments = (array) $query->posts;
+	
+		foreach( $attachments as $attachment_id ) {
+			$attachment = new Imagify_Attachment( $attachment_id );
+	
+			// Check if the attachment extension is allowed
+			if ( ! wp_attachment_is_image( $attachment_id ) ) {
+				continue;
+			}
+	
+			$stats_data    = $attachment->get_stats_data();
+			$original_data = $attachment->get_size_data( 'full' );
+	
+			// Incremente the original sizes
+			if ( $attachment->is_optimized() ) {
+				$original_size  += ( $original_data['original_size'] ) ? $original_data['original_size'] : 0;
+				$optimized_size += ( $original_data['optimized_size'] ) ? $original_data['optimized_size'] : 0;
+			}
+			
+			$metadata = wp_get_attachment_metadata( $attachment_id );
+			$sizes    = ( isset( $metadata['sizes'] ) ) ? (array) $metadata['sizes'] : array();
+	
+			// Incremente the thumbnails sizes
+			foreach ( $sizes as $size_key => $size_data ) {
+				$size_data = $attachment->get_size_data( $size_key );
+				if ( ! empty( $size_data['success'] ) ) {
+					$original_size  += ( $size_data['original_size'] ) ? $size_data['original_size'] : 0;
+					$optimized_size += ( $size_data['optimized_size'] ) ? $size_data['optimized_size'] : 0;
+				}
 			}
 		}
+	
+		$data = array(
+			'count'			 => $query->post_count,
+			'original_size'  => (int) $original_size,
+			'optimized_size' => (int) $optimized_size,
+			'percent'		 => ( 0 !== $optimized_size ) ? ceil( ( ( $original_size - $optimized_size ) / $original_size ) * 100 ) : 0
+		);
+		set_transient( $transient, $data );
 	}
-
-	$data = array(
-		'count'			 => $query->post_count,
-		'original_size'  => (int) $original_size,
-		'optimized_size' => (int) $optimized_size,
-		'percent'		 => ( 0 !== $optimized_size ) ? ceil( ( ( $original_size - $optimized_size ) / $original_size ) * 100 ) : 0
-	);
-
+	
 	if ( ! empty( $key ) ) {
 		return $data[ $key ];
 	}
