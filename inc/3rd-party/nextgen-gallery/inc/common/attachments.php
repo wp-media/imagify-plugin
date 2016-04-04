5aba006c9ae420e3f953784e8577ef50fed3b6ad
Jonathan Buttigieg <jonathan.buttigieg@yahoo.fr>
2016-04-04 17:03:25 +0200

    Auto-optimize an image added on a gallery

--- inc/3rd-party/nextgen-gallery/inc/common/attachments.php
+++ inc/3rd-party/nextgen-gallery/inc/common/attachments.php
@@ -2,6 +2,27 @@
 defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
 
 /**
+ * Auto-optimize when a new attachment is generated
+ *
+ * @since 1.5
+ * @author Jonathan Buttigieg
+ */
+add_action( 'ngg_after_new_images_added', '_imagify_ngg_optimize_attachment', PHP_INT_MAX, 2 );
+function _imagify_ngg_optimize_attachment( $galleryID, $image_ids ) {
+	$api_key = get_imagify_option( 'api_key', false );
+
+	if ( ! empty( $api_key ) && get_imagify_option( 'auto_optimize', false ) ) {		
+		
+		foreach ( $image_ids as $id ) {
+			$attachment = new Imagify_NGG_Attachment( $id );		
+		
+			// Optimize it!!!!!
+			$attachment->optimize( null );	
+		}
+	}
+}
+
+/**
  * Delete the Imagify data when an image is deleted.
  *
  * @since 1.5
