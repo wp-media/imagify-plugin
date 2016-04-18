426b86b9291e718a89e78147dd064562ddc33937
Jonathan Buttigieg <jonathan.buttigieg@yahoo.fr>
2016-04-18 12:01:56 +0200

    Fix FUCKING NGG bug to load our stats in the right way

--- inc/3rd-party/nextgen-gallery/inc/admin/bulk.php
+++ inc/3rd-party/nextgen-gallery/inc/admin/bulk.php
@@ -7,12 +7,14 @@ defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
  * @since 1.5
  * @author Jonathan Buttigieg
  */
-add_action( 'load-gallery_page_imagify-ngg-bulk-optimization', '_imagify_ngg_update_bulk_stats' );
-function _imagify_ngg_update_bulk_stats() {
-	add_filter( 'imagify_count_attachments' 			, 'imagify_ngg_count_attachments' );	
-	add_filter( 'imagify_count_optimized_attachments' 	, 'imagify_ngg_count_optimized_attachments' );	
-	add_filter( 'imagify_count_error_attachments'	  	, 'imagify_ngg_count_error_attachments' );
-	add_filter( 'imagify_count_unoptimized_attachments'	, 'imagify_ngg_count_unoptimized_attachments' );
-	add_filter( 'imagify_percent_optimized_attachments'	, 'imagify_ngg_percent_optimized_attachments' );
-	add_filter( 'imagify_count_saving_data'	  		  	, 'imagify_ngg_count_saving_data' );
+add_action( 'admin_init', '_imagify_ngg_update_bulk_stats' );
+function _imagify_ngg_update_bulk_stats() {	
+	if ( isset( $_GET['page'] ) && 'imagify-ngg-bulk-optimization' === $_GET['page'] ) {
+		add_filter( 'imagify_count_attachments' 			, 'imagify_ngg_count_attachments' );	
+		add_filter( 'imagify_count_optimized_attachments' 	, 'imagify_ngg_count_optimized_attachments' );
+		add_filter( 'imagify_count_error_attachments'	  	, 'imagify_ngg_count_error_attachments' );
+		add_filter( 'imagify_count_unoptimized_attachments'	, 'imagify_ngg_count_unoptimized_attachments' );
+		add_filter( 'imagify_percent_optimized_attachments'	, 'imagify_ngg_percent_optimized_attachments' );
+		add_filter( 'imagify_count_saving_data'	  		  	, 'imagify_ngg_count_saving_data' );
+	}
 }
