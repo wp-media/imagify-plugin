2ca676586e4c15184281feed3aaf5fbf3041a97a
Jonathan Buttigieg <jonathan.buttigieg@yahoo.fr>
2016-04-18 18:15:18 +0200

    Fix issue between Bulk Optimization & WP Engine queries limit

--- inc/3rd-party/3rd-party.php
+++ inc/3rd-party/3rd-party.php
@@ -3,3 +3,6 @@ defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
 
 require( IMAGIFY_3RD_PARTY_PATH . 'enable-media-replace.php' );
 require( IMAGIFY_3RD_PARTY_PATH . 'wp-retina-2x.php' );
+
+// Hosting
+require( IMAGIFY_3RD_PARTY_PATH . 'hosting/wpengine.php' );
