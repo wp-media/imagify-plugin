37d51726fc7421f49052f06e26e8f2b67618b3f2
Geoffrey Crofte <support@creativejuiz.com>
2016-09-15 12:28:54 +0200

    Revert "Only display the new to imagify banner if user has a valid key"
    This reverts commit 8f19fa5bbe8c005082520f8f13ecc747a1ea37cd.

--- inc/admin/ui/options.php
+++ inc/admin/ui/options.php
@@ -73,9 +73,7 @@ function _imagify_display_options_page() {
 
 				<h3 class="screen-reader-text"><?php _e( 'Settings' ); ?></h3>
 
-				<?php if ( imagify_valid_key() ) {
-    				echo get_imagify_new_to_imagify();
-                } ?>
+				<?php echo get_imagify_new_to_imagify(); ?>
 
 				<?php
 				if ( ! defined( 'IMAGIFY_API_KEY' ) || ! IMAGIFY_API_KEY ) { ?>
