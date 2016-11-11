771ff667fb7d2694a48c666336b245da868278e6
Jean-Baptiste Marchand-Arvier <jb.marchandarvier@gmail.com>
2016-11-11 15:44:10 +0100

    Update to the good url

--- imagify.php
+++ imagify.php
@@ -21,7 +21,7 @@ define( 'IMAGIFY_VERSION'	 			 , '1.5.10' );
 define( 'IMAGIFY_SLUG'		 			 , 'imagify' );
 define( 'IMAGIFY_SETTINGS_SLUG'			 , IMAGIFY_SLUG . '_settings' );
 define( 'IMAGIFY_WEB_MAIN'	 			 , 'https://imagify.io' );
-define( 'IMAGIFY_APP_MAIN'	 			 , 'http://dapp.imagify.io' );
+define( 'IMAGIFY_APP_MAIN'	 			 , 'https://app.imagify.io' );
 define( 'IMAGIFY_PAYMENT_URL'            , IMAGIFY_APP_MAIN . '/#/plugin/' );
 define( 'IMAGIFY_FILE'            		 , __FILE__ );
 define( 'IMAGIFY_PATH'       			 , realpath( plugin_dir_path( IMAGIFY_FILE ) ) . '/' );
