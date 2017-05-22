55a9f83c609db56e412c90789d52ef2a9c0a3a94
Git I Hate You <git@screenfeed.fr>
2017-05-23 00:03:10 +0200

    WPCS for `imagify.php`, `uninstall.php`, and `inc/compat.php`.

--- inc/compat.php
+++ inc/compat.php
@@ -1,20 +1,23 @@
 <?php
-defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
+defined( 'ABSPATH' ) || die( 'Cheatin\' uh?' );
 
-/**
- * Create a CURLFile object.
- *
- * @since PHP 5.5
- *
- * @param 	string $filename  Path to the file which will be uploaded.
- * @param 	string $mimetype  Mimetype of the file.
- * @param 	string $postname  Name of the file to be used in the upload data.
- * @return string 			  The CURLFile object.
- */
-if ( ! function_exists( 'curl_file_create' ) ) {
-    function curl_file_create( $filename, $mimetype = '', $postname = '' ) {
-        return "@$filename;filename="
-            . ( $postname ? $postname : basename( $filename ) )
-            . ( $mimetype ? ";type=$mimetype" : '' );
-    }
-}
+if ( ! function_exists( 'curl_file_create' ) ) :
+
+	/**
+	 * Create a CURLFile object.
+	 *
+	 * @since 1.0
+	 * @since PHP 5.5
+	 *
+	 * @param  string $filename Path to the file which will be uploaded.
+	 * @param  string $mimetype Mimetype of the file.
+	 * @param  string $postname Name of the file to be used in the upload data.
+	 * @return string           The CURLFile object.
+	 */
+	function curl_file_create( $filename, $mimetype = '', $postname = '' ) {
+		return "@$filename;filename="
+			. ( $postname ? $postname : basename( $filename ) )
+			. ( $mimetype ? ";type=$mimetype" : '' );
+	}
+
+endif;
--- uninstall.php
+++ uninstall.php
@@ -1,29 +1,29 @@
 <?php
-// If uninstall not called from WordPress exit
-defined( 'WP_UNINSTALL_PLUGIN' ) or die( 'Cheatin&#8217; uh?' );
+defined( 'WP_UNINSTALL_PLUGIN' ) || die( 'Cheatin&#8217; uh?' );
 
-// Delete Imagify options
+global $wpdb;
+
+// Delete Imagify options.
 delete_site_option( 'imagify_settings' );
 
-// Delete all transients
+// Delete all transients.
 delete_site_transient( 'imagify_check_licence_1' );
 delete_site_transient( 'imagify_bulk_optimization_level' );
 delete_site_transient( 'imagify_large_library' );
 delete_site_transient( 'imagify_max_image_size' );
 
-// Clear scheduled hooks
+// Clear scheduled hooks.
 wp_clear_scheduled_hook( 'imagify_rating_event' );
 wp_clear_scheduled_hook( 'imagify_update_library_size_calculations_event' );
 
-global $wpdb;
-// WP transients
+// WP transients.
 $wpdb->query( 'DELETE from ' . $wpdb->options . ' WHERE option_name LIKE "_transient_imagify-async-in-progress-%"' );
 
-// NextGen Gallery transients
+// NextGen Gallery transients.
 $wpdb->query( 'DELETE from ' . $wpdb->options . ' WHERE option_name LIKE "_transient_imagify-ngg-async-in-progress-%"' );
 
-// Delete all user meta related to Imagify
+// Delete all user meta related to Imagify.
 delete_metadata( 'user', '', '_imagify_ignore_notices', '', true );
 
-// Drop the tables
-$wpdb->query( "DROP TABLE IF EXISTS $wpdb->ngg_imagify_data" );
+// Drop the tables.
+$wpdb->query( 'DROP TABLE IF EXISTS ' . $wpdb->ngg_imagify_data );
