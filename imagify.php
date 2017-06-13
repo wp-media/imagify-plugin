cde3a166d2478cda529e66e0b13f595263470cad
Git I Hate You <git@screenfeed.fr>
2017-06-13 16:40:21 +0200

    Moved and renamed `inc/classes/class-imagify-deprecated.php` into `inc/functions/deprecated.php`. Edited `phpcs.xml` accordingly.

--- imagify.php
+++ imagify.php
@@ -60,6 +60,7 @@ function _imagify_init() {
 
 	require( IMAGIFY_INC_PATH . 'compat.php' );
 	require( IMAGIFY_API_PATH . 'imagify.php' );
+	require( IMAGIFY_FUNCTIONS_PATH . 'deprecated.php' );
 	require( IMAGIFY_FUNCTIONS_PATH . 'common.php' );
 	require( IMAGIFY_FUNCTIONS_PATH . 'options.php' );
 	require( IMAGIFY_FUNCTIONS_PATH . 'formatting.php' );
@@ -73,7 +74,6 @@ function _imagify_init() {
 	require( IMAGIFY_FUNCTIONS_PATH . 'i18n.php' );
 	require( IMAGIFY_CLASSES_ABSTRACTS_PATH . 'class-imagify-abstract-db.php' );
 	require( IMAGIFY_CLASSES_ABSTRACTS_PATH . 'class-imagify-abstract-attachment.php' );
-	require( IMAGIFY_CLASSES_PATH . 'class-imagify-deprecated.php' );
 	require( IMAGIFY_CLASSES_PATH . 'class-imagify.php' );
 	require( IMAGIFY_CLASSES_PATH . 'class-imagify-user.php' );
 	require( IMAGIFY_CLASSES_PATH . 'class-imagify-attachment.php' );
