73e8663cc70320f69df2dee261fe9701fd6a26dd
Git I Hate You <git@screenfeed.fr>
2017-06-30 16:03:54 +0200

    Revamped `imagify_ngg_count_saving_data()` to handle huge libraries, the same way we dealt with `imagify_count_saving_data()`.

--- inc/3rd-party/nextgen-gallery/inc/functions/admin-stats.php
+++ inc/3rd-party/nextgen-gallery/inc/functions/admin-stats.php
@@ -87,21 +87,108 @@ function imagify_ngg_percent_optimized_attachments() {
 	$total_attachments           = imagify_ngg_count_attachments();
 	$total_optimized_attachments = imagify_ngg_count_optimized_attachments();
 
-	return ( 0 !== $total_attachments ) ? round( 100 - ( ( $total_attachments - $total_optimized_attachments ) / $total_attachments ) * 100 ) : 0;
+	return $total_attachments && $total_optimized_attachments ? round( 100 - ( ( $total_attachments - $total_optimized_attachments ) / $total_attachments ) * 100 ) : 0;
 }
 
 /**
  * Count percent, original & optimized size of all images optimized by Imagify.
  *
- * @since 1.5
+ * @since  1.5
+ * @since  1.6.7 Revamped to handle huge libraries.
  * @author Jonathan Buttigieg
  *
- * @return array An array containing the optimization data.
+ * @param  bool|array $attachments An array containing the keys 'count', 'original_size', and 'optimized_size', or an array of attachments (back compat', deprecated), or false.
+ * @return array An array containing the keys 'count', 'original_size', and 'optimized_size'.
  */
-function imagify_ngg_count_saving_data() {
+function imagify_ngg_count_saving_data( $attachments ) {
 	global $wpdb;
 
-	$table_name = $wpdb->ngg_imagify_data;
+	if ( is_array( $attachments ) ) {
+		return $attachments;
+	}
 
-	return $wpdb->get_col( "SELECT $table_name.data FROM {$wpdb->ngg_imagify_data} WHERE status = 'success'" ); // WPCS: unprepared SQL ok.
+	/**
+	 * Filter the query to get all optimized NGG attachments.
+	 * 3rd party will be able to override the result.
+	 *
+	 * @since 1.6.7
+	 *
+	 * @param bool|array $attachments An array containing the keys ('count', 'original_size', and 'optimized_size'), or false.
+	 */
+	$attachments = apply_filters( 'imagify_ngg_count_saving_data', false );
+
+	if ( is_array( $attachments ) ) {
+		return $attachments;
+	}
+
+	$table_name     = $wpdb->ngg_imagify_data;
+	$original_size  = 0;
+	$optimized_size = 0;
+	$count          = 0;
+
+	/** This filter is documented in /inc/functions/admin-stats.php */
+	$limit  = apply_filters( 'imagify_count_saving_data_limit', 15000 );
+	$limit  = absint( $limit );
+	$offset = 0;
+
+	$attachments = $wpdb->get_col( // WPCS: unprepared SQL ok.
+		"SELECT $table_name.data
+		 FROM {$wpdb->ngg_imagify_data}
+		 WHERE status = 'success'
+		 LIMIT $offset, $limit"
+	);
+	$wpdb->flush();
+
+	while ( $attachments ) {
+		$attachments = array_map( 'maybe_unserialize', $attachments );
+
+		foreach ( $attachments as $attachment_data ) {
+			if ( ! $attachment_data ) {
+				continue;
+			}
+
+			++$count;
+			$original_data = $attachment_data['sizes']['full'];
+
+			// Increment the original sizes.
+			$original_size  += $original_data['original_size']  ? $original_data['original_size']  : 0;
+			$optimized_size += $original_data['optimized_size'] ? $original_data['optimized_size'] : 0;
+
+			unset( $attachment_data['sizes']['full'], $original_data );
+
+			// Increment the thumbnails sizes.
+			foreach ( $attachment_data['sizes'] as $size_data ) {
+				if ( ! empty( $size_data['success'] ) ) {
+					$original_size  += $size_data['original_size']  ? $size_data['original_size']  : 0;
+					$optimized_size += $size_data['optimized_size'] ? $size_data['optimized_size'] : 0;
+				}
+			}
+
+			unset( $size_data );
+		}
+
+		unset( $attachment_data );
+
+		if ( count( $attachments ) === $limit ) {
+			// Unless we are really unlucky, we still have attachments to fetch.
+			$offset += $limit;
+
+			$attachments = $wpdb->get_col( // WPCS: unprepared SQL ok.
+				"SELECT $table_name.data
+				 FROM {$wpdb->ngg_imagify_data}
+				 WHERE status = 'success'
+				 LIMIT $offset, $limit"
+			);
+			$wpdb->flush();
+		} else {
+			// Save one request, don't go back to the beginning of the loop.
+			$attachments = array();
+		}
+	} // End while().
+
+	return array(
+		'count'          => $count,
+		'original_size'  => $original_size,
+		'optimized_size' => $optimized_size,
+	);
 }
