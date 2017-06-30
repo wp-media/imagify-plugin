507343a6500fe20b46549407a982817ff45dedd9
Git I Hate You <git@screenfeed.fr>
2017-06-30 15:48:49 +0200

    Set the filter `'imagify_count_saving_data'` priority to `8` when filtering with `imagify_ngg_count_saving_data()`, to make sure to hook before non-NGG-specific filters.

--- inc/3rd-party/nextgen-gallery/inc/admin/bulk.php
+++ inc/3rd-party/nextgen-gallery/inc/admin/bulk.php
@@ -18,5 +18,5 @@ function _imagify_ngg_update_bulk_stats() {
 	add_filter( 'imagify_count_error_attachments'       , 'imagify_ngg_count_error_attachments' );
 	add_filter( 'imagify_count_unoptimized_attachments' , 'imagify_ngg_count_unoptimized_attachments' );
 	add_filter( 'imagify_percent_optimized_attachments' , 'imagify_ngg_percent_optimized_attachments' );
-	add_filter( 'imagify_count_saving_data'             , 'imagify_ngg_count_saving_data' );
+	add_filter( 'imagify_count_saving_data'             , 'imagify_ngg_count_saving_data', 8 );
 }
