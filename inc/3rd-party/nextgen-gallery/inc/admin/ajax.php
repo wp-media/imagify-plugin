dbd2e87881af1c09093c6366e344214415578255
Git I Hate You <git@screenfeed.fr>
2017-07-10 12:27:43 +0200

    After previous revert, bring back some code improvements.

--- inc/3rd-party/nextgen-gallery/inc/admin/ajax.php
+++ inc/3rd-party/nextgen-gallery/inc/admin/ajax.php
@@ -58,12 +58,13 @@ function _do_wp_ajax_imagify_ngg_get_unoptimized_attachment_ids() {
 	set_transient( 'imagify_bulk_optimization_level', $optimization_level );
 
 	foreach ( $images as $image ) {
-		$id = $image['id'];
+		$id        = absint( $image['id'] );
+		$file_path = $storage->get_image_abspath( $id );
 
 		/** This filter is documented in inc/functions/process.php. */
-		$file_path = apply_filters( 'imagify_file_path', $storage->get_image_abspath( $id ) );
+		$file_path = apply_filters( 'imagify_file_path', $file_path );
 
-		if ( ! file_exists( $file_path ) ) {
+		if ( ! $file_path || ! file_exists( $file_path ) ) {
 			continue;
 		}
 
