fc1e6c6700d2a55655298fe25ac504467e1eeacb
Git I Hate You <git@screenfeed.fr>
2017-07-17 11:20:54 +0200

    Allow to (de)select ALL thumbnail sizes at once in the settings page.

--- assets/js/options.js
+++ assets/js/options.js
@@ -121,3 +121,70 @@ window.imagify = window.imagify || {
 	} );
 
 } )(jQuery, document, window);
+
+
+// "Select all" checkboxes =========================================================================
+(function( w, d, $, undefined ) {
+
+	var lastClicked = {},
+		jqPropHookChecked = $.propHooks.checked;
+
+	// Force `.prop()` to trigger a `change` event.
+	$.propHooks.checked = {
+		set: function( elem, value, name ) {
+			var ret;
+
+			if ( undefined === jqPropHookChecked ) {
+				ret = ( elem[ name ] = value );
+			} else {
+				ret = jqPropHookChecked( elem, value, name );
+			}
+
+			$( elem ).trigger( 'change.imagify' );
+
+			return ret;
+		}
+	};
+
+	// Check all checkboxes.
+	$( '.imagify-check-group .imagify-row-check' ).on( 'click', function( e ) {
+		var $group     = $( this ).closest( '.imagify-check-group' ),
+			allChecked = 0 === $group.find( '.imagify-row-check' ).filter( ':visible:enabled' ).not( ':checked' ).length;
+
+		// Toggle "check all" checkboxes.
+		$group.find( '.imagify-toggle-check' ).prop( 'checked', allChecked );
+	} )
+	.first().trigger( 'change.imagify' );
+
+	$( '.imagify-check-group .imagify-toggle-check' ).on( 'click.wp-toggle-checkboxes', function( e ) {
+		var $this          = $( this ),
+			$wrap          = $this.closest( '.imagify-check-group' ),
+			controlChecked = $this.prop( 'checked' ),
+			toggle         = e.shiftKey || $this.data( 'wp-toggle' );
+
+		$wrap.find( '.imagify-toggle-check' )
+			.prop( 'checked', function() {
+				var $this = $( this );
+
+				if ( $this.is( ':hidden,:disabled' ) ) {
+					return false;
+				}
+
+				if ( toggle ) {
+					return ! $this.prop( 'checked' );
+				}
+
+				return controlChecked ? true : false;
+			} );
+
+		$wrap.find( '.imagify-row-check' )
+			.prop( 'checked', function() {
+				if ( toggle ) {
+					return false;
+				}
+
+				return controlChecked ? true : false;
+			} );
+	} );
+
+} )(window, document, jQuery);
