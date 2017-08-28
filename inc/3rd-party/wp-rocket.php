6b130bc141694c5d545a040dc8e7092fe2e7f463
Git I Hate You <git@screenfeed.fr>
2017-08-28 16:08:05 +0200

    Hotfix: PHPCS in `/inc/3rd-party/wp-rocket.php`.

--- inc/3rd-party/wp-rocket.php
+++ inc/3rd-party/wp-rocket.php
@@ -1,21 +1,26 @@
 <?php
 defined( 'ABSPATH' ) || die( 'Cheatin\' uh?' );
 
-// temp fix on 1.6.9.1
-if ( defined( 'WP_ROCKET_VERSION' ) ) {
+/**
+ * Temp fix on 1.6.9.1.
+ */
+if ( defined( 'WP_ROCKET_VERSION' ) ) :
+
+	add_action( 'admin_print_styles', 'imagify_dequeue_sweetalert_wprocket', 11 );
 	/**
-	 * Don't load Imagify CSS & JS files on WP Rocket options screen to avoid conflict.
+	 * Don't load Imagify CSS & JS files on WP Rocket options screen to avoid conflict with older version of SweetAlert..
 	 *
 	 * @since 1.6.9.1
 	 * @author Jonathan Buttigieg
 	 */
-	add_action( 'admin_print_styles', 'imagify_dequeue_sweetalert_wprocket', 11 );
 	function imagify_dequeue_sweetalert_wprocket() {
 		$current_screen = get_current_screen();
+
 		if ( isset( $current_screen ) && ( 'settings_page_wprocket' === $current_screen->base || 'settings_page_wprocket-network' === $current_screen->base ) ) {
 			wp_dequeue_style( 'imagify-css-sweetalert' );
 			wp_dequeue_script( 'imagify-js-admin' );
 			wp_dequeue_script( 'imagify-js-sweetalert' );
 		}
 	}
-}
+
+endif;
