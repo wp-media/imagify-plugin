a91c0cf20855ec2d8736bfb17b6081c1587dfdef
Git I Hate You <git@screenfeed.fr>
2017-09-27 11:11:20 +0200

    Hotfix: moved `imagify_check_nonce()`, `imagify_check_user_capacity()`, `imagify_die()`, and `imagify_maybe_redirect()` from */inc/functions/common.php* to */inc/functions/admin.php*. Since they are only used in admin ajax/post callbacks, it seems more logical.

--- inc/functions/admin.php
+++ inc/functions/admin.php
@@ -272,3 +272,123 @@ function imagify_get_wp_rocket_url( $path = false, $query = array() ) {
 
 	return add_query_arg( $query, $wprocket_url );
 }
+
+/**
+ * Check for nonce.
+ *
+ * @since  1.6.10
+ * @author Grégory Viguier
+ *
+ * @param string      $action Action nonce.
+ * @param string|bool $query_arg Optional. Key to check for the nonce in `$_REQUEST`. If false, `$_REQUEST` values will be evaluated for '_ajax_nonce', and '_wpnonce' (in that order). Default false.
+ */
+function imagify_check_nonce( $action, $query_arg = false ) {
+	if ( ! check_ajax_referer( $action, $query_arg, false ) ) {
+		imagify_die();
+	}
+}
+
+/**
+ * Check for user capacity.
+ *
+ * @since  1.6.10
+ * @author Grégory Viguier
+ *
+ * @param string $capacity a user capacity.
+ */
+function imagify_check_user_capacity( $capacity = null ) {
+	if ( ! isset( $capacity ) ) {
+		$capacity = imagify_get_capacity();
+	}
+
+	if ( ! current_user_can( $capacity ) ) {
+		imagify_die();
+	}
+}
+
+/**
+ * Die Today.
+ *
+ * @since  1.6.10
+ * @author Grégory Viguier
+ *
+ * @param string $message A message to display.
+ */
+function imagify_die( $message = null ) {
+	if ( ! isset( $message ) ) {
+		/* translators: This sentense already exists in WordPress. */
+		$message = __( 'Sorry, you are not allowed to do that.', 'imagify' );
+	} elseif ( is_wp_error( $message ) ) {
+		$message = imagify_translate_api_message( $message->get_error_message() );
+	}
+
+	if ( is_array( $message ) ) {
+		if ( ! empty( $message['error'] ) ) {
+			$message['error']  = imagify_translate_api_message( $message['error'] );
+		} elseif ( ! empty( $message['detail'] ) ) {
+			$message['detail'] = imagify_translate_api_message( $message['detail'] );
+		}
+	}
+
+	if ( defined( 'DOING_AJAX' ) ) {
+		wp_send_json_error( $message );
+	}
+
+	if ( is_array( $message ) ) {
+		if ( ! empty( $message['error'] ) ) {
+			$message = $message['error'];
+		} elseif ( ! empty( $message['detail'] ) ) {
+			$message = $message['detail'];
+		} else {
+			$message = reset( $message );
+		}
+	}
+
+	if ( wp_get_referer() ) {
+		$message .= '</p><p>';
+		$message .= sprintf( '<a href="%s">%s</a>',
+			esc_url( remove_query_arg( 'updated', wp_get_referer() ) ),
+			/* translators: This sentense already exists in WordPress. */
+			__( 'Go back', 'imagify' )
+		);
+	}
+
+	/* translators: %s is the plugin name. */
+	wp_die( $message, sprintf( __( '%s Failure Notice', 'imagify' ), 'Imagify' ), 403 );
+}
+
+/**
+ * Redirect if not an ajax request.
+ *
+ * @since  1.6.10
+ * @author Grégory Viguier
+ *
+ * @param string       $message     A message to display in an admin notice once redirected.
+ * @param array|string $args_or_url An array of query args to add to the redirection URL. If a string, the complete URL.
+ */
+function imagify_maybe_redirect( $message = false, $args_or_url = array() ) {
+	if ( defined( 'DOING_AJAX' ) ) {
+		return;
+	}
+
+	if ( $args_or_url && is_array( $args_or_url ) ) {
+		$redirect = add_query_arg( $args_or_url, wp_get_referer() );
+	} elseif ( $args_or_url && is_string( $args_or_url ) ) {
+		$redirect = $args_or_url;
+	} else {
+		$redirect = wp_get_referer();
+	}
+
+	/**
+	 * Filter the URL to redirect to.
+	 *
+	 * @since  1.6.10
+	 * @author Grégory Viguier
+	 *
+	 * @param string $redirect The URL to redirect to.
+	 */
+	$redirect = apply_filters( 'imagify_redirect_to', $redirect );
+
+	wp_safe_redirect( esc_url_raw( $redirect ) );
+	die();
+}
