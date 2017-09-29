f27fe7445a07d87a2374d73499ea7af066c0d195
Git I Hate You <git@screenfeed.fr>
2017-09-29 14:50:53 +0200

    `imagify_get_capacity()` now uses a "user capacity describer", like 'manage', 'bulk-optimize', 'manual-optimize', and 'auto-optimize'. It provides 3 main pros: - In the plugin, something like `imagify_get_capacity( 'bulk-optimize' )` is more understandable than `imagify_get_capacity( true )`. - It's a way to centralize all the logic behind user capacities. - It can also be used for `'upload_files'` now. Back compat' with true/false is assured.

--- inc/functions/common.php
+++ inc/functions/common.php
@@ -5,26 +5,46 @@ defined( 'ABSPATH' ) || die( 'Cheatin\' uh?' );
  * Get user capacity to operate Imagify.
  *
  * @since  1.6.5
+ * @since  1.6.11 Uses a string as describer for the first argument.
  * @author Grégory Viguier
  *
- * @param  bool $force_mono Force capacity for mono-site.
+ * @param  string $describer Capacity describer. Possible values are 'manage', 'bulk-optimize', 'manual-optimize', and 'auto-optimize'.
  * @return string
  */
-function imagify_get_capacity( $force_mono = false ) {
-	if ( $force_mono || ! is_multisite() ) {
-		$capacity = 'manage_options';
-	} else {
-		$capacity = imagify_is_active_for_network() ? 'manage_network_options' : 'manage_options';
+function imagify_get_capacity( $describer = 'manage' ) {
+	// Back compat.
+	if ( ! is_string( $describer ) ) {
+		if ( $describer || ! is_multisite() ) {
+			$describer = 'bulk-optimize';
+		} else {
+			$describer = 'manage';
+		}
+	}
+
+	switch ( $describer ) {
+		case 'manage':
+			$capacity = imagify_is_active_for_network() ? 'manage_network_options' : 'manage_options';
+			break;
+		case 'bulk-optimize':
+			$capacity = 'manage_options';
+			break;
+		case 'manual-optimize':
+		case 'auto-optimize':
+			$capacity = 'upload_files';
+			break;
+		default:
+			$capacity = $describer;
 	}
 
 	/**
 	 * Filter the user capacity used to operate Imagify.
 	 *
 	 * @since 1.0
-	 * @since 1.6.5 Added $force_mono parameter.
+	 * @since 1.6.5  Added $force_mono parameter.
+	 * @since 1.6.11 Replaced $force_mono by $describer.
 	 *
-	 * @param string $capacity   The user capacity.
-	 * @param bool   $force_mono Force capacity for mono-site.
+	 * @param string $capacity  The user capacity.
+	 * @param string $describer Capacity describer. Possible values are 'manage', 'bulk-optimize', 'manual-optimize', and 'auto-optimize'.
 	 */
-	return apply_filters( 'imagify_capacity', $capacity, $force_mono );
+	return apply_filters( 'imagify_capacity', $capacity, $describer );
 }
