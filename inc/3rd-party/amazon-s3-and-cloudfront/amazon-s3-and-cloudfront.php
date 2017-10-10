772b72ad6847665fbd9010b8d7bffb8637ba6225
Git I Hate You <git@screenfeed.fr>
2017-10-11 00:43:33 +0200

    Removed all class file inclusions and autoload classes. Also removed constants `IMAGIFY_CLASSES_ABSTRACTS_PATH` and `IMAGIFY_NGG_CLASSES_PATH`. Finally, no need to instantiate `Imagify_NGG_DB` right away.

--- inc/3rd-party/amazon-s3-and-cloudfront/amazon-s3-and-cloudfront.php
+++ inc/3rd-party/amazon-s3-and-cloudfront/amazon-s3-and-cloudfront.php
@@ -3,9 +3,6 @@ defined( 'ABSPATH' ) || die( 'Cheatin\' uh?' );
 
 if ( is_admin() && ( function_exists( 'as3cf_init' ) || function_exists( 'as3cf_pro_init' ) ) ) :
 
-	require( dirname( __FILE__ ) . '/inc/classes/class-imagify-as3cf.php' );
-	require( dirname( __FILE__ ) . '/inc/classes/class-imagify-as3cf-attachment.php' );
-
-	add_action( 'imagify_loaded', array( imagify_as3cf(), 'init' ), 1 );
+	add_action( 'imagify_loaded', array( Imagify_AS3CF::get_instance(), 'init' ), 1 );
 
 endif;
