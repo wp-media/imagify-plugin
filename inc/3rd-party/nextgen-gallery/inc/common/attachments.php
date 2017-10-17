6c0fba2a33b1ca87f40d3444e05d3060050739d8
Git I Hate You <git@screenfeed.fr>
2017-10-17 14:48:49 +0200

    Hotfix: introduced filter `imagify_auto_optimize_ngg_gallery_image` to do the same job than `imagify_auto_optimize_attachment` for regular attachment.

--- inc/3rd-party/nextgen-gallery/inc/common/attachments.php
+++ inc/3rd-party/nextgen-gallery/inc/common/attachments.php
@@ -5,26 +5,41 @@ add_action( 'ngg_after_new_images_added', '_imagify_ngg_optimize_attachment', IM
 /**
  * Auto-optimize when a new attachment is generated.
  *
- * @since 1.5
+ * @since  1.5
  * @author Jonathan Buttigieg
  *
  * @param int   $gallery_id A Gallery ID.
  * @param array $image_ids  Id's which are sucessfully added.
  */
 function _imagify_ngg_optimize_attachment( $gallery_id, $image_ids ) {
-	$api_key = get_imagify_option( 'api_key', false );
 
-	if ( ! $api_key || ! get_imagify_option( 'auto_optimize', false ) ) {
+	if ( ! imagify_valid_key() || ! get_imagify_option( 'auto_optimize' ) ) {
 		return;
 	}
 
-	foreach ( $image_ids as $id ) {
+	foreach ( $image_ids as $image_id ) {
+		/**
+		 * Allow to prevent automatic optimization for a specific NGG gallery image.
+		 *
+		 * @since  1.6.12
+		 * @author Grégory Viguier
+		 *
+		 * @param bool $optimize   True to optimize, false otherwise.
+		 * @param int  $image_id   Image ID.
+		 * @param int  $gallery_id Gallery ID.
+		 */
+		$optimize = apply_filters( 'imagify_auto_optimize_ngg_gallery_image', true, $image_id, $gallery_id );
+
+		if ( ! $optimize ) {
+			continue;
+		}
+
 		imagify_do_async_job( array(
 			'action'        => 'imagify_async_optimize_upload_new_media',
-			'_ajax_nonce'   => wp_create_nonce( 'new_media-' . $id ),
+			'_ajax_nonce'   => wp_create_nonce( 'new_media-' . $image_id ),
 			'metadata'      => 1,
 			'context'       => 'NGG',
-			'attachment_id' => $id,
+			'attachment_id' => $image_id,
 		) );
 	}
 }
@@ -33,7 +48,7 @@ add_action( 'ngg_delete_picture', '_imagify_ngg_delete_picture' );
 /**
  * Delete the Imagify data when an image is deleted.
  *
- * @since 1.5
+ * @since  1.5
  * @author Jonathan Buttigieg
  *
  * @param int $image_id An image ID.
@@ -46,11 +61,11 @@ add_filter( 'ngg_medialibrary_imported_image', '_imagify_ngg_media_library_impor
 /**
  * Import Imagify data from a WordPress image to a new NGG image
  *
- * @since 1.5
+ * @since  1.5
  * @author Jonathan Buttigieg
  *
- * @param object $image      A NGG image object.
- * @param object $attachment An attachment object.
+ * @param  object $image      A NGG image object.
+ * @param  object $attachment An attachment object.
  * @return object
  */
 function _imagify_ngg_media_library_imported_image_data( $image, $attachment ) {
