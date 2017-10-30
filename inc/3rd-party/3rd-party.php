6e72c592d490247278b95b23ab51d2dd8450d659
Git I Hate You <git@screenfeed.fr>
2017-10-31 00:24:51 +0100

    Compatibility with SiteGround: change the User-Agent for HTTP requests "to self".

--- inc/3rd-party/3rd-party.php
+++ inc/3rd-party/3rd-party.php
@@ -10,4 +10,5 @@ require( IMAGIFY_3RD_PARTY_PATH . 'wp-rocket.php' );
 require( IMAGIFY_3RD_PARTY_PATH . 'yoast-seo.php' );
 
 // Hosting.
+require( IMAGIFY_3RD_PARTY_PATH . 'hosting/siteground.php' );
 require( IMAGIFY_3RD_PARTY_PATH . 'hosting/wpengine.php' );
