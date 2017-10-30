3a391013d586dbfa8942453b3515172a6dd1e103
Git I Hate You <git@screenfeed.fr>
2017-10-30 16:39:51 +0100

    Upgrader: - Don't create default options on first install, this is useless now. - Make sure plugin settings are autoloaded.

--- inc/admin/upgrader.php
+++ inc/admin/upgrader.php
@@ -22,10 +22,7 @@ function _imagify_upgrader() {
 
 	// If any upgrade has been done, we flush and update version.
 	if ( did_action( 'imagify_first_install' ) || did_action( 'imagify_upgrade' ) ) {
-		$options            = get_site_option( IMAGIFY_SETTINGS_SLUG ); // Do not use get_imagify_option() here.
-		$options['version'] = IMAGIFY_VERSION;
-
-		update_site_option( IMAGIFY_SETTINGS_SLUG, $options );
+		update_imagify_option( 'version', IMAGIFY_VERSION );
 	}
 }
 
@@ -38,19 +35,6 @@ add_action( 'imagify_first_install', '_imagify_first_install' );
 function _imagify_first_install() {
 	// Set a transient to know when we will have to display a notice to ask the user to rate the plugin.
 	set_site_transient( 'imagify_seen_rating_notice', true, DAY_IN_SECONDS * 3 );
-
-	// Create Options.
-	add_site_option( IMAGIFY_SETTINGS_SLUG, array(
-		'api_key'            => '',
-		'optimization_level' => 1,
-		'auto_optimize'      => 1,
-		'backup'             => 1,
-		'resize_larger'      => '',
-		'resize_larger_w'    => '',
-		'exif'               => 0,
-		'disallowed-sizes'   => array(),
-		'admin_bar_menu'     => 1,
-	) );
 }
 
 add_action( 'imagify_upgrade', '_imagify_new_upgrade', 10, 2 );
@@ -64,6 +48,8 @@ add_action( 'imagify_upgrade', '_imagify_new_upgrade', 10, 2 );
  */
 function _imagify_new_upgrade( $imagify_version, $current_version ) {
 	global $wpdb;
+
+	$options = Imagify_Options::get_instance();
 
 	// 1.2
 	if ( version_compare( $current_version, '1.2' ) < 0 ) {
@@ -91,11 +77,7 @@ function _imagify_new_upgrade( $imagify_version, $current_version ) {
 		}
 
 		// Auto-activate the Admin Bar option.
-		$options = get_site_option( IMAGIFY_SETTINGS_SLUG );
-		$options = is_array( $options ) ? $options : array();
-		$options['admin_bar_menu'] = 1;
-
-		update_site_option( IMAGIFY_SETTINGS_SLUG, $options );
+		$options->set( 'admin_bar_menu', 1 );
 	}
 
 	// 1.3.2
@@ -137,5 +119,11 @@ function _imagify_new_upgrade( $imagify_version, $current_version ) {
 	if ( version_compare( $current_version, '1.4.5' ) < 0 ) {
 		// Delete all transients used for async optimization.
 		$wpdb->query( 'DELETE from ' . $wpdb->options . ' WHERE option_name LIKE "_transient_imagify-async-in-progress-%"' );
+	}
+
+	// 1.6.13
+	if ( ! imagify_is_active_for_network() && $options->is_autoloaded() && version_compare( $current_version, '1.6.13' ) < 0 ) {
+		// Make sure the settings are auloaded.
+		$wpdb->query( $wpdb->prepare( "UPDATE {$wpdb->options} SET `autoload` = 'yes' WHERE `autoload` != 'yes' AND option_name = %s", $options->get_option_name() ) );
 	}
 }
