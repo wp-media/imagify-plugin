03610924de6405f33c1b5c8d9b0d4cf46319bc2c
Git I Hate You <git@screenfeed.fr>
2017-12-13 18:25:10 +0100

    Added the views list to the "files list" template, and a hidden input for the "page" parameter.

--- inc/views/page-files-list.php
+++ inc/views/page-files-list.php
@@ -6,6 +6,8 @@ defined( 'ABSPATH' ) || die( 'Cheatin\' uh?' );
 	<?php $this->print_template( 'part-files-list-header' ); ?>
 
 		<form id="imagify-files-list-form" method="get">
+			<input type="hidden" name="page" value="<?php echo esc_attr( Imagify_Views::get_instance()->get_files_page_slug() ); ?>"/>
+			<?php $this->list_table->views(); ?>
 			<?php $this->list_table->display(); ?>
 		</form>
 
