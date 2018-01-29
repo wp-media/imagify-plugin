a9ca625df45fe3ab00a3b5c25457808b58f5452a
Git I Hate You <git@screenfeed.fr>
2018-01-30 00:13:58 +0100

    PHPCS + added some `esc_html()` while at it.

--- inc/views/part-settings-account.php
+++ inc/views/part-settings-account.php
@@ -7,7 +7,7 @@ if ( imagify_valid_key() ) {
 	$hidden_class     = '';
 
 	if ( ! $user ) {
-		// Lazyload user
+		// Lazyload user.
 		Imagify_Assets::get_instance()->localize_script( 'options', 'imagifyUser', array(
 			'action'   => 'imagify_get_user_data',
 			'_wpnonce' => wp_create_nonce( 'imagify_get_user_data' ),
@@ -20,11 +20,11 @@ if ( imagify_valid_key() ) {
 <div class="imagify-settings-section">
 	<?php if ( imagify_valid_key() ) { ?>
 		<h2 class="imagify-options-title">
-			<?php _e( 'Account Type', 'imagify' ); ?>
+			<?php esc_html_e( 'Account Type', 'imagify' ); ?>
 			<strong class="imagify-user-plan-label"><?php echo $user ? esc_html( $user->plan_label ) : ''; ?></strong>
 		</h2>
 	<?php } else { ?>
-		<h2 class="imagify-options-title"><?php _e( 'Your account', 'imagify' ); ?></h2>
+		<h2 class="imagify-options-title"><?php esc_html_e( 'Your account', 'imagify' ); ?></h2>
 	<?php } ?>
 
 	<?php
@@ -35,14 +35,14 @@ if ( imagify_valid_key() ) {
 		$options = Imagify_Options::get_instance();
 		?>
 		<div class="imagify-api-line">
-			<label for="api_key"><?php _e( 'API Key', 'imagify' ); ?></label>
+			<label for="api_key"><?php esc_html_e( 'API Key', 'imagify' ); ?></label>
 			<input type="text" size="35" value="<?php echo esc_attr( $options->get( 'api_key' ) ); ?>" name="<?php echo $options->get_option_name(); ?>[api_key]" id="api_key">
 			<?php
 			if ( imagify_valid_key() ) {
 				?>
 
 				<span id="imagify-check-api-container" class="imagify-valid">
-					<span class="imagify-icon">✓</span> <?php _e( 'Your API key is valid.', 'imagify' ); ?>
+					<span class="imagify-icon">✓</span> <?php esc_html_e( 'Your API key is valid.', 'imagify' ); ?>
 				</span>
 
 				<?php
@@ -50,7 +50,7 @@ if ( imagify_valid_key() ) {
 				?>
 
 				<span id="imagify-check-api-container">
-					<span class="dashicons dashicons-no"></span> <?php _e( 'Your API key isn\'t valid!', 'imagify' ); ?>
+					<span class="dashicons dashicons-no"></span> <?php esc_html_e( 'Your API key isn\'t valid!', 'imagify' ); ?>
 				</span>
 
 				<?php
@@ -60,7 +60,7 @@ if ( imagify_valid_key() ) {
 				echo '<p class="description desc api_key">';
 				printf(
 					/* translators: 1 is a link tag start, 2 is the link tag end. */
-					__( 'Don\'t have an API Key yet? %1$sCreate one, it\'s FREE%2$s.', 'imagify' ),
+					esc_html__( 'Don\'t have an API Key yet? %1$sCreate one, it\'s FREE%2$s.', 'imagify' ),
 					'<a id="imagify-signup" href="' . esc_url( imagify_get_external_url( 'register' ) ) . '" target="_blank">',
 					'</a>'
 				);
@@ -84,9 +84,9 @@ if ( imagify_valid_key() ) {
 					echo '<div class="imagify-user-is-almost-over-quota hidden">';
 				}
 				?>
-				<p><strong><?php _e( 'Oops, It\'s almost over!', 'imagify' ); ?></strong></p>
-				<p><?php _e( 'You have almost used all your credit. Don\'t forget to upgrade your subscription to continue optimizing your images.', 'imagify' ); ?></p>
-				<p><a class="button imagify-button-ghost" href="<?php echo esc_url( imagify_get_external_url( 'subscription' ) ); ?>" target="_blank"><?php _e( 'View My Subscription', 'imagify' ); ?></a></p>
+				<p><strong><?php esc_html_e( 'Oops, It\'s almost over!', 'imagify' ); ?></strong></p>
+				<p><?php esc_html_e( 'You have almost used all your credit. Don\'t forget to upgrade your subscription to continue optimizing your images.', 'imagify' ); ?></p>
+				<p><a class="button imagify-button-ghost" href="<?php echo esc_url( imagify_get_external_url( 'subscription' ) ); ?>" target="_blank"><?php esc_html_e( 'View My Subscription', 'imagify' ); ?></a></p>
 				<?php
 				if ( ! $user ) {
 					echo '</div>';
@@ -98,18 +98,20 @@ if ( imagify_valid_key() ) {
 					echo '<div class="imagify-user-is-over-quota hidden">';
 				}
 				?>
-				<p><strong><?php _e( 'Oops, It\'s Over!', 'imagify' ); ?></strong></p>
+				<p><strong><?php esc_html_e( 'Oops, It\'s Over!', 'imagify' ); ?></strong></p>
 				<p>
 					<?php
 					printf(
-						/* translators: 1 is a data quota, 2 is a date. */
-						__( 'You have consumed all your credit for this month. You will have <strong>%1$s back on %2$s</strong>.', 'imagify' ),
+						/* translators: 1 is a "bold" tag openning, 2 is a data quota, 3 is a date, 4 is the "bold" tag closing. */
+						esc_html__( 'You have consumed all your credit for this month. You will have %1$s%2$s back on %3$s%4$s.', 'imagify' ),
+						'<strong>',
 						'<span class="imagify-user-quota-formatted">' . ( $user ? esc_html( $user->quota_formatted ) : '' ) . '</span>',
-						'<span class="imagify-user-next-date-update-formatted">' . ( $user ? esc_html( $user->next_date_update_formatted ) : '' ) . '</span>'
+						'<span class="imagify-user-next-date-update-formatted">' . ( $user ? esc_html( $user->next_date_update_formatted ) : '' ) . '</span>',
+						'</strong>'
 					);
 					?>
 				</p>
-				<p class="center txt-center text-center"><a class="btn imagify-btn-ghost" href="<?php echo esc_url( imagify_get_external_url( 'subscription' ) ); ?>" target="_blank"><?php _e( 'Upgrade My Subscription', 'imagify' ); ?></a></p>
+				<p class="center txt-center text-center"><a class="btn imagify-btn-ghost" href="<?php echo esc_url( imagify_get_external_url( 'subscription' ) ); ?>" target="_blank"><?php esc_html_e( 'Upgrade My Subscription', 'imagify' ); ?></a></p>
 				<?php
 				if ( ! $user ) {
 					echo '</div>';
@@ -121,16 +123,16 @@ if ( imagify_valid_key() ) {
 			 */
 			?>
 			<div class="best-plan<?php echo $hidden_class; ?>">
-				<p><?php _e( 'We can also analyze your needs to prevent you to buy a plan that doesn\'t suit you.', 'imagify' ); ?><br>
-				<?php _e( 'Our analyze will allow you to choose a better plan for your needs.', 'imagify' ); ?></p>
+				<p><?php esc_html_e( 'We can also analyze your needs to prevent you to buy a plan that doesn\'t suit you.', 'imagify' ); ?><br>
+				<?php esc_html_e( 'Our analyze will allow you to choose a better plan for your needs.', 'imagify' ); ?></p>
 
-				<h3><?php _e( 'You’re new to Imagify', 'imagify' ); ?></h3>
+				<h3><?php esc_html_e( 'You’re new to Imagify', 'imagify' ); ?></h3>
 
-				<p><?php _e( 'Let us help you by analyzing your existing images and determine the best plan for you.', 'imagify' ); ?></p>
+				<p><?php esc_html_e( 'Let us help you by analyzing your existing images and determine the best plan for you.', 'imagify' ); ?></p>
 
 				<button id="imagify-get-pricing-modal" data-nonce="<?php echo wp_create_nonce( 'imagify_get_pricing_' . get_current_user_id() ); ?>" data-target="#imagify-pricing-modal" type="button" class="imagify-modal-trigger imagify-button imagify-button-secondary">
 					<i class="dashicons dashicons-dashboard" aria-hidden="true"></i>
-					<span class="button-text"><?php _e( 'What plan do I need?', 'imagify' ); ?></span>
+					<span class="button-text"><?php esc_html_e( 'What plan do I need?', 'imagify' ); ?></span>
 				</button>
 			</div>
 		</div><!-- .imagify-col-content -->
