81b37ba644df65fdf638693a184d6d67038db1f6
Git I Hate You <git@screenfeed.fr>
2018-03-05 15:46:20 +0100

    Don't use HTML entities like `&nbsp;` and `&amp;` in translations.

--- inc/views/part-settings-footer.php
+++ inc/views/part-settings-footer.php
@@ -11,7 +11,7 @@ defined( 'ABSPATH' ) || die( 'Cheatin\' uh?' );
 	if ( imagify_valid_key() ) {
 		// Submit and go to bulk page.
 		submit_button(
-			esc_html__( 'Save &amp; Go to Bulk Optimizer', 'imagify' ),
+			esc_html__( 'Save & Go to Bulk Optimizer', 'imagify' ),
 			'secondary imagify-button-secondary', // Type/classes.
 			'submit-goto-bulk', // Name (id).
 			true, // Wrap.
