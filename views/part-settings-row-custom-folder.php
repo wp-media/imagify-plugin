96ebf8ce18d8cde8a5c995fde16daec1a99a62d7
Git I Hate You <git@screenfeed.fr>
2018-03-08 11:52:22 +0100

    Hotfix: added i18n context.

--- views/part-settings-row-custom-folder.php
+++ views/part-settings-row-custom-folder.php
@@ -4,5 +4,5 @@ defined( 'ABSPATH' ) || die( 'Cheatin\' uh?' );
 <p class="imagify-custom-folder-line" data-path="<?php echo '{{ABSPATH}}/' === $data['value'] ? '/' : esc_attr( $data['label'] ); ?>">
 	<input type="hidden" name="imagify_settings[custom_folders][]" value="<?php echo esc_attr( $data['value'] ); ?>" />
 	<?php echo esc_html( $data['label'] ); ?>
-	<button type="button" class="imagify-custom-folders-remove"><span class="imagify-custom-folders-remove-text"><?php _e( 'Remove', 'imagify' ); ?></span><i class="dashicons dashicons-no-alt" aria-hidden="true"></i></button>
+	<button type="button" class="imagify-custom-folders-remove"><span class="imagify-custom-folders-remove-text"><?php _ex( 'Remove', 'custom folder', 'imagify' ); ?></span><i class="dashicons dashicons-no-alt" aria-hidden="true"></i></button>
 </p>
