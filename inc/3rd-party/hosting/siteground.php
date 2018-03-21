c83531278c59862251cb37177be2a84b2cd93613
Git I Hate You <git@screenfeed.fr>
2018-03-21 23:59:46 +0100

    For internal http requests, change the User-Agent only for useful URLs.

--- inc/3rd-party/hosting/siteground.php
+++ inc/3rd-party/hosting/siteground.php
@@ -3,9 +3,7 @@ defined( 'ABSPATH' ) || die( 'Cheatin\' uh?' );
 
 add_filter( 'http_request_args', 'imagify_siteground_change_user_agent', 10, 2 );
 /**
- * Filter the arguments used in a HTTP request to change the User Agent for requests "to self".
- * SiteGround blocks (error 403) HTTP requests with a User-Agent containing the site's domain.
- * The problem is that, for a given site at https://example.com, WordPress uses the UA `WordPress/4.8.2; https://example.com`.
+ * Filter the arguments used in a HTTP request to change the User Agent for requests "to self", to prevent firewalls to be triggered.
  *
  * @since  1.6.13
  * @author Grégory Viguier
@@ -16,20 +14,55 @@ add_filter( 'http_request_args', 'imagify_siteground_change_user_agent', 10, 2 )
  */
 function imagify_siteground_change_user_agent( $r, $url ) {
 	static $user_agent;
-	$site_url = site_url( '/' );
+	static $site_url;
 
-	if ( false === strpos( $url, $site_url ) ) {
+	$url = wp_parse_url( $url );
+
+	if ( empty( $url['path'] ) ) {
 		return $r;
 	}
 
-	$site_url = wp_parse_url( $site_url );
+	$paths = array(
+		'/wp-admin/admin-ajax.php',
+		'/wp-admin/admin-post.php',
+		'/wp-cron.php',
+	);
 
-	if ( false === strpos( $r['user-agent'], $site_url['host'] ) ) {
+	if ( ! isset( $site_url ) ) {
+		$site_url = wp_parse_url( site_url( '/' ) );
+	}
+
+	// Limit to requests to self.
+	if ( false === strpos( $url['path'], $site_url['path'] ) && false === strpos( $site_url['path'], $url['path'] ) ) {
 		return $r;
 	}
 
+	// Limit to requests to admin-ajax.php, admin-post.php, and wp-cron.php.
+	foreach ( $paths as $i => $path ) {
+		if ( false !== strpos( $url['path'], $path ) ) {
+			$paths = false;
+			break;
+		}
+	}
+
+	if ( $paths ) {
+		return $r;
+	}
+
+	// Randomize the User-Agent.
 	if ( ! isset( $user_agent ) ) {
 		$user_agent = wp_generate_password( 12, false );
+		/**
+		 * Filter the User-Agent used for requests "to self".
+		 *
+		 * @since  1.7.1
+		 * @author Grégory Viguier
+		 *
+		 * @param string $user_agent The User-Agent.
+		 * @param array  $r          An array of HTTP request arguments.
+		 * @param array  $url        The request URL, parsed.
+		 */
+		$user_agent = apply_filters( 'imagify_user_agent_for_internal_requests', $user_agent, $r, $url );
 	}
 
 	$r['user-agent'] = $user_agent;
