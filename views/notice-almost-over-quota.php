234b82dd9a0fe7371b29f93c4a054276a0e3eb27
Git I Hate You <git@screenfeed.fr>
2018-07-10 01:12:08 +0200

    Use new methods in the "almost over quota" notice

--- views/notice-almost-over-quota.php
+++ views/notice-almost-over-quota.php
@@ -5,27 +5,24 @@ $this->print_template( 'notice-header', array(
 	'classes' => array( 'imagify-flex-notice-content', 'error' ),
 ) );
 
-$user              = new Imagify_User();
-$unconsumed_quota  = $user->get_percent_unconsumed_quota();
-$meteo_icon        = '<img src="' . IMAGIFY_ASSETS_IMG_URL . 'stormy.svg" width="64" height="63" alt="" />';
-$bar_class         = 'negative';
+$views = Imagify_Views::get_instance();
 ?>
 <div class="imagify-notice-quota">
 	<div class="imagify-flex imagify-vcenter">
-		<span class="imagify-meteo-icon imagify-noshrink"><?php echo $meteo_icon; ?></span>
+		<span class="imagify-meteo-icon imagify-noshrink"><?php echo $views->get_quota_icon(); ?></span>
 		<div class="imagify-space-left">
 			<p>
 				<?php
 				printf(
 					/* translators: %s is a data quota. */
 					esc_html__( 'You have %s space credit left' , 'imagify' ),
-					'<span class="imagify-unconsumed-percent">' . $unconsumed_quota . '%</span>'
+					'<span class="imagify-unconsumed-percent">' . $views->get_quota_percent() . '%</span>'
 				);
 				?>
 			</p>
 
-			<div class="imagify-bar-<?php echo $bar_class; ?>">
-				<div class="imagify-unconsumed-bar imagify-progress" style="width: <?php echo $unconsumed_quota . '%'; ?>;"></div>
+			<div class="<?php echo $views->get_quota_class(); ?>">
+				<div class="imagify-unconsumed-bar imagify-progress" style="width: <?php echo $views->get_quota_percent() . '%'; ?>;"></div>
 			</div>
 		</div>
 	</div>
