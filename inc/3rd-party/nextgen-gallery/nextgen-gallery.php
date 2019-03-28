4a5449a4100ff04124521fa05f1bcda95475a3f4
Git I Hate You <git@screenfeed.fr>
2019-03-28 16:51:05 +0100

    Don't use `IMAGIFY_3RD_PARTY_PATH` anymore

--- inc/3rd-party/nextgen-gallery/nextgen-gallery.php
+++ inc/3rd-party/nextgen-gallery/nextgen-gallery.php
@@ -11,19 +11,19 @@ class_alias( '\\Imagify\\ThirdParty\\NGG\\Main', '\\Imagify_NGG' );
 class_alias( '\\Imagify\\ThirdParty\\NGG\\DB', '\\Imagify_NGG_DB' );
 class_alias( '\\Imagify\\ThirdParty\\NGG\\NGGStorage', '\\Imagify_NGG_Storage' );
 
-require IMAGIFY_3RD_PARTY_PATH . 'nextgen-gallery/inc/functions/admin-stats.php';
-require IMAGIFY_3RD_PARTY_PATH . 'nextgen-gallery/inc/functions/attachments.php';
-require IMAGIFY_3RD_PARTY_PATH . 'nextgen-gallery/inc/functions/common.php';
-require IMAGIFY_3RD_PARTY_PATH . 'nextgen-gallery/inc/common/attachments.php';
+require IMAGIFY_PATH . 'inc/3rd-party/nextgen-gallery/inc/functions/admin-stats.php';
+require IMAGIFY_PATH . 'inc/3rd-party/nextgen-gallery/inc/functions/attachments.php';
+require IMAGIFY_PATH . 'inc/3rd-party/nextgen-gallery/inc/functions/common.php';
+require IMAGIFY_PATH . 'inc/3rd-party/nextgen-gallery/inc/common/attachments.php';
 
 NGG\Main::get_instance()->init();
 NGG\DB::get_instance()->init();
 
 if ( is_admin() ) {
-	require IMAGIFY_3RD_PARTY_PATH . 'nextgen-gallery/inc/admin/enqueue.php';
-	require IMAGIFY_3RD_PARTY_PATH . 'nextgen-gallery/inc/admin/ajax.php';
-	require IMAGIFY_3RD_PARTY_PATH . 'nextgen-gallery/inc/admin/menu.php';
-	require IMAGIFY_3RD_PARTY_PATH . 'nextgen-gallery/inc/admin/gallery.php';
-	require IMAGIFY_3RD_PARTY_PATH . 'nextgen-gallery/inc/admin/bulk.php';
-	require IMAGIFY_3RD_PARTY_PATH . 'nextgen-gallery/inc/admin/heartbeat.php';
+	require IMAGIFY_PATH . 'inc/3rd-party/nextgen-gallery/inc/admin/enqueue.php';
+	require IMAGIFY_PATH . 'inc/3rd-party/nextgen-gallery/inc/admin/ajax.php';
+	require IMAGIFY_PATH . 'inc/3rd-party/nextgen-gallery/inc/admin/menu.php';
+	require IMAGIFY_PATH . 'inc/3rd-party/nextgen-gallery/inc/admin/gallery.php';
+	require IMAGIFY_PATH . 'inc/3rd-party/nextgen-gallery/inc/admin/bulk.php';
+	require IMAGIFY_PATH . 'inc/3rd-party/nextgen-gallery/inc/admin/heartbeat.php';
 }
