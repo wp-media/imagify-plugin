2f80b2d00118387cb94f33943cf02f4821cd62f0
Git I Hate You <git@screenfeed.fr>
2019-03-28 16:18:18 +0100

    Webp compat’ with Enable Media Replace plugin

--- inc/3rd-party/enable-media-replace/enable-media-replace.php
+++ inc/3rd-party/enable-media-replace/enable-media-replace.php
@@ -3,6 +3,8 @@ defined( 'ABSPATH' ) || die( 'Cheatin’ uh?' );
 
 if ( function_exists( 'enable_media_replace' ) ) :
 
-	add_filter( 'emr_unfiltered_get_attached_file', array( Imagify_Enable_Media_Replace::get_instance(), 'init' ) );
+	class_alias( '\\Imagify\\ThirdParty\\EnableMediaReplace\\Main', '\\Imagify_Enable_Media_Replace' );
+
+	add_filter( 'emr_unfiltered_get_attached_file', [ \Imagify\ThirdParty\EnableMediaReplace\Main::get_instance(), 'init' ] );
 
 endif;
