27b2f82d1e9071dc345d371f5c1a5f20e9dd78d1
Git I Hate You <git@screenfeed.fr>
2019-09-26 00:51:42 +0200

    Display the "Save & Go to Bulk Optimizer" only if the user can bulk optimize

--- views/part-settings-footer.php
+++ views/part-settings-footer.php
@@ -9,14 +9,18 @@ defined( 'ABSPATH' ) || die( 'Cheatin’ uh?' );
 	submit_button();
 
 	if ( Imagify_Requirements::is_api_key_valid() ) {
-		// Submit and go to bulk page.
-		submit_button(
-			esc_html__( 'Save & Go to Bulk Optimizer', 'imagify' ),
-			'secondary imagify-button-secondary', // Type/classes.
-			'submit-goto-bulk', // Name (id).
-			true, // Wrap.
-			array() // Other attributes.
-		);
+		$user_can = imagify_get_context( 'wp' )->current_user_can( 'bulk-optimize' ) || imagify_get_context( 'custom-folders' )->current_user_can( 'bulk-optimize' );
+
+		if ( $user_can ) {
+			// Submit and go to bulk page.
+			submit_button(
+				esc_html__( 'Save & Go to Bulk Optimizer', 'imagify' ),
+				'secondary imagify-button-secondary', // Type/classes.
+				'submit-goto-bulk', // Name (id).
+				true, // Wrap.
+				array() // Other attributes.
+			);
+		}
 	}
 	?>
 </div>
