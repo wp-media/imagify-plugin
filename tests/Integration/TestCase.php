2ffdee1976659c599e68557fccfec4f517fe96bb
Git I Hate You <git@screenfeed.fr>
2020-01-27 22:54:03 +0100

    Integration tests class - Use the right namespace. - New `getApiCredential()` method.

--- tests/Integration/TestCase.php
+++ tests/Integration/TestCase.php
@@ -2,10 +2,10 @@
 /**
  * Test Case for all of the integration tests.
  *
- * @package Imagify\Tests\Integration
+ * @package Imagify\tests\Integration
  */
 
-namespace Imagify\Tests\Integration;
+namespace Imagify\tests\Integration;
 
 use Brain\Monkey;
 use Imagify\Tests\TestCaseTrait;
@@ -13,6 +13,14 @@ use WP_UnitTestCase;
 
 abstract class TestCase extends WP_UnitTestCase {
 	use TestCaseTrait;
+
+	/**
+	 * Name of the API credentials config file, if applicable. Set in the test or new TestCase.
+	 * Example: 'imagify-api'.
+	 *
+	 * @var string
+	 */
+	protected $api_credentials_config_file;
 
 	/**
 	 * Prepares the test environment before each test.
@@ -28,5 +36,42 @@ abstract class TestCase extends WP_UnitTestCase {
 	public function tearDown() {
 		Monkey\tearDown();
 		parent::tearDown();
+	}
+
+	/**
+	 * Gets the credential's value from either an environment variable (stored locally on the machine or CI) or from a local constant defined in `tests/env/local/imagify-api.php`.
+	 *
+	 * @param  string $name Name of the environment variable or constant to find. Example: 'IMAGIFY_TESTS_API_KEY'.
+	 * @return string       Return the value if available. An empty string otherwise.
+	 */
+	protected function getApiCredential( $name ) {
+		$var = getenv( $name );
+
+		if ( ! empty( $var ) ) {
+			return $var;
+		}
+
+		if ( defined( $name ) ) {
+			return constant( $name );
+		}
+
+		if ( ! $this->api_credentials_config_file ) {
+			return '';
+		}
+
+		$config_file = dirname( __DIR__ ) . '/env/local/' . $this->api_credentials_config_file . '.php';
+
+		if ( ! is_readable( $config_file ) ) {
+			return '';
+		}
+
+		// This file is local to the developer's machine and not stored in the repo.
+		require_once $config_file;
+
+		if ( ! defined( $name ) ) {
+			return '';
+		}
+
+		return constant( $name );
 	}
 }
