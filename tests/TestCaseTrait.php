459cd7084e450529487bf2516bdaff6892a3632c
Git I Hate You <git@screenfeed.fr>
2020-01-28 23:15:35 +0100

    Improved TestCaseTrait trait - Improved method `setSingletonInstance()` + use `instance` property instead of `_instance`. - Added methods `reetPropertyValue()` and `etPropertyValue()`. - Renamed `getStaticPropertyValue()` into `getPropertyValue()`, as it can be used for both static and instance properties. - Small improvements to DocBlocks.

--- tests/TestCaseTrait.php
+++ tests/TestCaseTrait.php
@@ -15,36 +15,83 @@ use ReflectionProperty;
 trait TestCaseTrait {
 
 	/**
-	 * Set the singleton's `$_instance` property to the given instance.
-	 *
-	 * @param string $class    Name of the target class.
-	 * @param mixed  $instance Instance of the target object.
+	 * Set the singleton's `$instance` property to the given instance.
 	 *
 	 * @throws ReflectionException Throws an exception if property does not exist.
+	 *
+	 * @param  string $class    Name of the target class.
+	 * @param  mixed  $instance Instance of the target object.
+	 * @return mixed            Previous value.
 	 */
 	protected function setSingletonInstance( $class, $instance ) {
-		$ref = $this->get_reflective_property( '_instance', $class );
-		$ref->setValue( $instance, $instance );
+		return $this->setPropertyValue( 'instance', $class, $instance );
 	}
 
-	protected function resetStaticProperty( $property, $class ) {
-		$ref = $this->get_reflective_property( $property, $class );
-		$ref->setValue( null );
+	/**
+	 * Reset the value of a private/protected property.
+	 *
+	 * @throws ReflectionException Throws an exception if property does not exist.
+	 *
+	 * @param  string        $property Property name for which to gain access.
+	 * @param  string|object $class    Class name for a static property, or instance for an instance property.
+	 * @return mixed                   The previous value of the property.
+	 */
+	protected function resetPropertyValue( $property, $class ) {
+		return $this->setPropertyValue( $property, $class, null );
 	}
 
-	protected function getStaticPropertyValue( $property, $class ) {
+	/**
+	 * Set the value of a private/protected property.
+	 *
+	 * @throws ReflectionException Throws an exception if property does not exist.
+	 *
+	 * @param  string        $property Property name for which to gain access.
+	 * @param  string|object $class    Class name for a static property, or instance for an instance property.
+	 * @param  mixed         $value    The value to set to the property.
+	 * @return mixed                   The previous value of the property.
+	 */
+	protected function setPropertyValue( $property, $class, $value ) {
+		$ref      = $this->get_reflective_property( $property, $class );
+		$previous = $ref->getValue();
+
+		if ( is_object( $class ) ) {
+			// Instance property.
+			$ref->setValue( $class, $value );
+		} else {
+			// Static property.
+			$ref->setValue( $value );
+		}
+
+		return $previous;
+	}
+
+	/**
+	 * Get the value of a private/protected property.
+	 *
+	 * @throws ReflectionException Throws an exception if property does not exist.
+	 *
+	 * @param  string        $property Property name for which to gain access.
+	 * @param  string|object $class    Class name for a static property, or instance for an instance property.
+	 * @return mixed
+	 */
+	protected function getPropertyValue( $property, $class ) {
 		$ref = $this->get_reflective_property( $property, $class );
 		return $ref->getValue();
 	}
 
+
+	/** ----------------------------------------------------------------------------------------- */
+	/** REFLECTIONS ============================================================================= */
+	/** ----------------------------------------------------------------------------------------- */
+
 	/**
-	 * Get reflective access to the private/protected method.
+	 * Get reflective access to a private/protected method.
 	 *
-	 * @param string $method_name Method name for which to gain access.
-	 * @param string $class_name  Name of the target class.
-	 *
-	 * @return ReflectionMethod
 	 * @throws ReflectionException Throws an exception if method does not exist.
+	 *
+	 * @param  string $method_name Method name for which to gain access.
+	 * @param  string $class_name  Name of the target class.
+	 * @return ReflectionMethod
 	 */
 	protected function get_reflective_method( $method_name, $class_name ) {
 		$class  = new ReflectionClass( $class_name );
@@ -55,13 +102,13 @@ trait TestCaseTrait {
 	}
 
 	/**
-	 * Get reflective access to the private/protected property.
+	 * Get reflective access to a private/protected property.
 	 *
-	 * @param string       $property Property name for which to gain access.
-	 * @param string|mixed $class    Class name or instance.
-	 *
-	 * @return ReflectionProperty|string
 	 * @throws ReflectionException Throws an exception if property does not exist.
+	 *
+	 * @param  string       $property Property name for which to gain access.
+	 * @param  string|mixed $class    Class name or instance.
+	 * @return ReflectionProperty
 	 */
 	protected function get_reflective_property( $property, $class ) {
 		$class    = new ReflectionClass( $class );
@@ -72,14 +119,14 @@ trait TestCaseTrait {
 	}
 
 	/**
-	 * Set the value of a property or private property.
+	 * Set the value of a private/protected property.
 	 *
-	 * @param mixed  $value    The value to set for the property.
-	 * @param string $property Property name for which to gain access.
-	 * @param mixed  $instance Instance of the target object.
-	 *
-	 * @return ReflectionProperty|string
 	 * @throws ReflectionException Throws an exception if property does not exist.
+	 *
+	 * @param  mixed  $value    The value to set for the property.
+	 * @param  string $property Property name for which to gain access.
+	 * @param  mixed  $instance Instance of the target object.
+	 * @return ReflectionProperty
 	 */
 	protected function set_reflective_property( $value, $property, $instance ) {
 		$property = $this->get_reflective_property( $property, $instance );
