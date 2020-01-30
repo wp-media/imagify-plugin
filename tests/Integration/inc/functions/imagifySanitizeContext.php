4e9455ba220e9bac4af3978ff20c51475491d4dd
Tonya Mork <hellofromtonya@knowthecode.io>
2020-01-30 11:36:29 -0600

    Removed test method DocBlock.

--- tests/Integration/inc/functions/imagifySanitizeContext.php
+++ tests/Integration/inc/functions/imagifySanitizeContext.php
@@ -5,9 +5,7 @@ namespace Imagify\tests\Integration\Functions;
 use Imagify\tests\Integration\TestCase;
 
 class Test_ImagifySanitizeContext extends TestCase {
-	/**
-	 * Test should return sanitized key.
-	 */
+
 	public function testShouldReturnSanitizedKey() {
 		$this->assertSame( 'httpsimagifyio', imagify_sanitize_context( 'https://imagify.io/' ) );
 		$this->assertSame( 'wpmediaimagify', imagify_sanitize_context( 'WPMedia Imagify' ) );
