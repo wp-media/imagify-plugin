06cc8e85b564e65fd1a45d5261d8451e83118310
Tonya Mork <hellofromtonya@knowthecode.io>
2020-01-30 11:51:53 -0600

    No need to run the API store/restore code in the function test.

--- Tests/Integration/inc/functions/imagifySanitizeContext.php
+++ Tests/Integration/inc/functions/imagifySanitizeContext.php
@@ -5,6 +5,7 @@ namespace Imagify\Tests\Integration\Functions;
 use Imagify\Tests\Integration\TestCase;
 
 class Test_ImagifySanitizeContext extends TestCase {
+	protected $useApi = false;
 
 	public function testShouldReturnSanitizedKey() {
 		$this->assertSame( 'httpsimagifyio', imagify_sanitize_context( 'https://imagify.io/' ) );
