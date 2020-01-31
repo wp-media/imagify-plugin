2e62b0ac924e54935249213c6f8fa8375480b812
Git I Hate You <git@screenfeed.fr>
2020-01-31 17:18:43 +0100

    Added custom error messages to unit tests

--- Tests/Unit/inc/admin/imagifyResetOpcache.php
+++ Tests/Unit/inc/admin/imagifyResetOpcache.php
@@ -33,52 +33,52 @@ class Test_ImagifyResetOpcache extends TestCase {
 
 	public function testShouldReturnTrueWhenOpcacheAvailable() {
 		if ( ! filter_var( ini_get( 'opcache.enable' ), FILTER_VALIDATE_BOOLEAN ) ) {
-			$this->assertFalse( imagify_reset_opcache( true ) );
+			$this->assertFalse( imagify_reset_opcache( true ), 'imagify_reset_opcache() returned true while OPcache is disabled.' );
 			return;
 		}
 
 		// Make sure OPcache is not empty.
 		ini_set( 'opcache.restrict_api', '' );
-		$this->assertTrue( @opcache_compile_file( $this->test_file_path ) );
-		$this->assertTrue( opcache_is_script_cached( $this->test_file_path ) );
+		$this->assertTrue( @opcache_compile_file( $this->test_file_path, 'Test file was not added to cache.' ) );
+		$this->assertTrue( opcache_is_script_cached( $this->test_file_path ), 'Test file is not cached.' );
 
 		// Now the real test.
-		$this->assertTrue( imagify_reset_opcache( true ) );
-		$this->assertFalse( opcache_is_script_cached( $this->test_file_path ) );
+		$this->assertTrue( imagify_reset_opcache( true ), 'imagify_reset_opcache() failed to reset OPcache.' );
+		$this->assertFalse( opcache_is_script_cached( $this->test_file_path ), 'Test file has not been removed from cache.' );
 	}
 
 	public function testShouldReturnFalseWhenOpcacheIsAvailable() {
 		if ( ! filter_var( ini_get( 'opcache.enable' ), FILTER_VALIDATE_BOOLEAN ) ) {
-			$this->assertFalse( imagify_reset_opcache( true ) );
+			$this->assertFalse( imagify_reset_opcache( true ), 'imagify_reset_opcache() returned true while OPcache is disabled.' );
 			return;
 		}
 
 		// Make sure OPcache is not empty.
 		ini_set( 'opcache.restrict_api', '' );
-		$this->assertTrue( @opcache_compile_file( $this->test_file_path ) );
-		$this->assertTrue( opcache_is_script_cached( $this->test_file_path ) );
+		$this->assertTrue( @opcache_compile_file( $this->test_file_path, 'Test file was not added to cache.' ) );
+		$this->assertTrue( opcache_is_script_cached( $this->test_file_path ), 'Test file is not cached.' );
 
 		// Now the real test: restrict.
 		ini_set( 'opcache.restrict_api', \dirname( $this->function_path ) );
 
 		// Enabled but restricted.
-		$this->assertFalse( imagify_reset_opcache( true ) );
-		$this->assertTrue( opcache_is_script_cached( $this->test_file_path ) );
+		$this->assertFalse( imagify_reset_opcache( true ), 'imagify_reset_opcache() reset OPcache while it should not have.' );
+		$this->assertTrue( opcache_is_script_cached( $this->test_file_path ), 'Test file has been removed from cache.' );
 		opcache_invalidate( $this->test_file_path );
 	}
 
 	public function testShouldReturnFalseWhenOpcacheNotAvailable() {
 		if ( filter_var( ini_get( 'opcache.enable' ), FILTER_VALIDATE_BOOLEAN ) ) {
-			$this->assertFalse( imagify_reset_opcache( true ) );
+			$this->assertFalse( imagify_reset_opcache( true ), 'imagify_reset_opcache() returned true while OPcache is disabled.' );
 			return;
 		}
 
 		// Disabled but not restricted.
 		ini_set( 'opcache.restrict_api', '' );
-		$this->assertFalse( imagify_reset_opcache( true ) );
+		$this->assertFalse( imagify_reset_opcache( true ), 'imagify_reset_opcache() reset OPcache while it should not have.' );
 
 		// Disabled and restricted.
 		ini_set( 'opcache.restrict_api', dirname( $this->function_path ) );
-		$this->assertFalse( imagify_reset_opcache( true ) );
+		$this->assertFalse( imagify_reset_opcache( true ), 'imagify_reset_opcache() reset OPcache while it should not have.' );
 	}
 }
