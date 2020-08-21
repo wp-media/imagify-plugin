6d587724ab72648f55e8f9640aebfb01d8769443
Caspar Green <cg@caspar.green>
2020-08-21 12:07:57 -0400

    Complete unit tests

--- Tests/Fixtures/inc/classes/ImagifySettings/updateSiteOptionOnNetwork.php
+++ Tests/Fixtures/inc/classes/ImagifySettings/updateSiteOptionOnNetwork.php
@@ -1,64 +1,80 @@
 <?php
 
 return [
-	'shouldBailWhenEmptyOptionPage' => [
-		'config'   => [
-			'option_page' => ''
+	'test_data' => [
+		'shouldBailWhenEmptyOptionPage' => [
+			'config'   => [
+				'option_page'     => '',
+				'user_can'        => true,
+				'nonce_check'     => true,
+				'missing_options' => false,
+				'options'         => []
+			],
+			'expected' => [],
 		],
-		'expected' => [],
-	],
 
-	'shouldBailWhenNotImagifySettingsPage' => [
-		'config'   => [
-			'option_page' => 'other-options-page',
+		'shouldBailWhenNotImagifySettingsPage' => [
+			'config'   => [
+				'option_page'     => 'other-options-page',
+				'user_can'        => true,
+				'nonce_check'     => true,
+				'missing_options' => false,
+				'options'         => []
+			],
+			'expected' => [],
 		],
-		'expected' => [],
-	],
 
-	'shouldBailWhenCurrentUserNotAuthorized' => [
-		'config'   => [
-			'option_page' => 'imagify',
-			'user_can'    => false,
+		'shouldBailWhenCurrentUserNotAuthorized' => [
+			'config'   => [
+				'option_page'     => 'imagify',
+				'user_can'        => false,
+				'nonce_check'     => true,
+				'missing_options' => false,
+				'options'         => []
+			],
+			'expected' => [],
 		],
-		'expected' => [],
-	],
 
-	'shouldBailWhenNonceCheckFails' => [
-		'config'   => [
-			'option_page' => 'imagify',
-			'user_can'    => true,
-			'nonce_check' => false,
+		'shouldBailWhenNonceCheckFails' => [
+			'config'   => [
+				'option_page'     => 'imagify',
+				'user_can'        => true,
+				'nonce_check'     => false,
+				'missing_options' => false,
+				'options'         => []
+			],
+			'expected' => [],
 		],
-		'expected' => [],
-	],
 
-	'shouldDieWithErrorWhenSettingsNotFound' => [
-		'config'   => [
-			'option_page'     => 'imagify',
-			'user_can'        => true,
-			'nonce_check'     => true,
-			'missing_options' => true,
+		'shouldDieWithErrorWhenSettingsNotFound' => [
+			'config'   => [
+				'option_page'     => 'imagify',
+				'user_can'        => true,
+				'nonce_check'     => true,
+				'missing_options' => true,
+				'options'         => []
+			],
+			'expected' => [
+				'die_message' => '<strong>ERROR</strong>: options page not found.',
+			],
 		],
-		'expected' => [
-			'die_message' => '<strong>ERROR</strong>: options page not found.',
-		],
-	],
 
-	'shouldUpdateOptions' => [
-		'config'   => [
-			'option_page'     => 'imagify',
-			'user_can'        => true,
-			'nonce_check'     => true,
-			'missing_options' => false,
-			'options'         => [
-				'optimization_level' => 1,
-				'backup'             => 1,
-				'convert_to_webp'    => 1,
-				'disallowed-sizes'   => [ 'thumbnail', 'large', 'medium' ]
-			]
+		'shouldUpdateOptions' => [
+			'config'   => [
+				'option_page'     => 'imagify',
+				'user_can'        => true,
+				'nonce_check'     => true,
+				'missing_options' => false,
+				'options'         => [
+					'optimization_level' => 1,
+					'backup'             => 1,
+					'convert_to_webp'    => 1,
+					'disallowed-sizes'   => [ 'thumbnail', 'large', 'medium' ]
+				],
+			],
+			'expected' => [
+				'options_count' => 4,
+			],
 		],
-		'expected' => [
-			'options_count' => 4,
-		],
-	],
+	]
 ];
--- Tests/Unit/inc/classes/ImagifySettings/updateSiteOptionOnNetwork.php
+++ Tests/Unit/inc/classes/ImagifySettings/updateSiteOptionOnNetwork.php
@@ -1,9 +1,10 @@
 <?php
 namespace Imagify\Tests\Unit\inc\classes\ImagifySettings;
 
+use Brain\Monkey\Filters;
 use Brain\Monkey\Functions;
-use Imagify\Tests\Unit\TestCase;
 use Imagify_Settings;
+use Imagify\Tests\Unit\TestCase;
 
 /**
  * @covers Imagify_Settings::update_site_option_on_network
@@ -12,18 +13,101 @@ use Imagify_Settings;
  * @uses   imagify_die()
  * @uses   imagify_maybe_redirect()
  *
- * @group  ImagifyAPI
+ * @group  ImagifySettings
  */
 class Test_UpdateSiteOptionOnNetwork extends TestCase {
 
-	/**
-	 * @dataProvider provideData
-	 */
-	public function shouldUpdateNetworkSettings( $config, $expected) {
-		var_dump( $config, $expected );
+	public function setUp() {
+		parent::setUp();
+
+		if ( ! defined( 'IMAGIFY_SLUG' ) ) {
+			define( 'IMAGIFY_SLUG', 'imagify' );
+		}
+
+		Functions\when( 'imagify_is_active_for_network' )->justReturn( true );
 	}
 
-	public function provideData() {
-		return $this->loadTestDataConfig();
+	/**
+	 * @dataProvider configTestData
+	 */
+	public function testShouldUpdateNetworkSettings( $config, $expected ) {
+		$_POST['option_page'] = $config['option_page'];
+
+		Functions\when( 'current_user_can' )->justReturn( $config['user_can'] );
+		Functions\when( 'imagify_check_nonce' )->justReturn( $config['nonce_check'] );
+
+		if ( empty( $config['option_page'] )
+			 || 'imagify' !== $config['option_page']
+		) {
+			$this->shouldBailOut( $config );
+		} elseif ( $config['missing_options']
+				   || ! $config['user_can']
+				   || ! $config['nonce_check']
+		) {
+			$this->shouldDie( $config );
+		} else {
+			$this->shouldUpdateOptions( $config, $expected );
+		}
+	}
+
+	public function shouldBailOut( $config ) {
+		if ( 'imagify' !== $config['option_page'] ) {
+			Filters\expectApplied( 'option_page_capability_imagify' )->never();
+		} else {
+			Filters\expectApplied( 'option_page_capability_imagify' )
+				->once()
+				->andReturn( $config['user_can'] );
+		}
+
+		Imagify_Settings::get_instance()->update_site_option_on_network();
+	}
+
+	public function shouldDie( $config ) {
+		Filters\expectApplied( 'option_page_capability_imagify' )
+			->once()
+			->andReturn( $config['user_can'] );
+		Functions\when( 'imagify_die' )
+			->justReturn();
+		Functions\when( 'imagify_check_nonce' )
+			->justReturn( $config['nonce_check'] );
+		Functions\when( 'apply_filters_deprecated' )
+			->justReturn();
+
+		if ( $config['missing_options'] ) {
+			Filters\expectApplied( 'allowed_options' )
+				->andReturn( $config['options'] );
+		}
+
+		Imagify_Settings::get_instance()->update_site_option_on_network();
+	}
+
+	public function shouldUpdateOptions( $config, $expected ) {
+		$options = [];
+
+		foreach ( $config['options'] as $option => $value ) {
+			$options[] = $option;
+			$_POST[ $option ] = $value;
+			Functions\expect( 'update_site_option' )
+				->once()
+				->with( $option, $value );
+		}
+
+		Filters\expectApplied( 'option_page_capability_imagify' )
+			->once()
+			->andReturn( true );
+		Functions\when( 'imagify_check_nonce' )
+			->justReturn( true );
+		Functions\when( 'apply_filters_deprecated' )
+			->justReturn();
+		Filters\expectApplied( 'allowed_options' )
+			->once()
+			->andReturn( [ 'imagify' => $options ] );
+		Functions\when('wp_unslash')->returnArg();
+
+		Functions\expect( 'imagify_maybe_redirect' )
+			->once()
+			->andReturn();
+
+		Imagify_Settings::get_instance()->update_site_option_on_network();
 	}
 }
