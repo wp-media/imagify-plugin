8ea0fa21ea6de3d22953a5371f93ebf2f906a048
Caspar Green <cg@caspar.green>
2020-08-21 17:11:40 -0400

    Reactivate fixtures and fix phpcs

--- Tests/Fixtures/inc/classes/ImagifySettings/updateSiteOptionOnNetwork.php
+++ Tests/Fixtures/inc/classes/ImagifySettings/updateSiteOptionOnNetwork.php
@@ -2,62 +2,62 @@
 
 return [
 	'test_data' => [
-//		'shouldBailWhenEmptyOptionPage' => [
-//			'config'   => [
-//				'option_page'     => '',
-//				'user_can'        => true,
-//				'nonce_check'     => true,
-//				'missing_options' => false,
-//				'options'         => []
-//			],
-//			'expected' => [],
-//		],
-//
-//		'shouldBailWhenNotImagifySettingsPage' => [
-//			'config'   => [
-//				'option_page'     => 'other-options-page',
-//				'user_can'        => true,
-//				'nonce_check'     => true,
-//				'missing_options' => false,
-//				'options'         => []
-//			],
-//			'expected' => [],
-//		],
-//
-//		'shouldDieWhenCurrentUserNotAuthorized' => [
-//			'config'   => [
-//				'option_page'     => 'imagify',
-//				'user_can'        => false,
-//				'nonce_check'     => true,
-//				'missing_options' => false,
-//				'options'         => []
-//			],
-//			'expected' => [],
-//		],
-//
-//		'shouldDieWhenNonceCheckFails' => [
-//			'config'   => [
-//				'option_page'     => 'imagify',
-//				'user_can'        => true,
-//				'nonce_check'     => false,
-//				'missing_options' => false,
-//				'options'         => []
-//			],
-//			'expected' => [],
-//		],
-//
-//		'shouldDieWithErrorWhenSettingsNotFound' => [
-//			'config'   => [
-//				'option_page'     => 'imagify',
-//				'user_can'        => true,
-//				'nonce_check'     => true,
-//				'missing_options' => true,
-//				'options'         => []
-//			],
-//			'expected' => [
-//				'die_message' => '<strong>ERROR</strong>: options page not found.',
-//			],
-//		],
+		'shouldBailWhenEmptyOptionPage' => [
+			'config'   => [
+				'option_page'     => '',
+				'user_can'        => true,
+				'nonce_check'     => true,
+				'missing_options' => false,
+				'options'         => []
+			],
+			'expected' => [],
+		],
+
+		'shouldBailWhenNotImagifySettingsPage' => [
+			'config'   => [
+				'option_page'     => 'other-options-page',
+				'user_can'        => true,
+				'nonce_check'     => true,
+				'missing_options' => false,
+				'options'         => []
+			],
+			'expected' => [],
+		],
+
+		'shouldDieWhenCurrentUserNotAuthorized' => [
+			'config'   => [
+				'option_page'     => 'imagify',
+				'user_can'        => false,
+				'nonce_check'     => true,
+				'missing_options' => false,
+				'options'         => []
+			],
+			'expected' => [],
+		],
+
+		'shouldDieWhenNonceCheckFails' => [
+			'config'   => [
+				'option_page'     => 'imagify',
+				'user_can'        => true,
+				'nonce_check'     => false,
+				'missing_options' => false,
+				'options'         => []
+			],
+			'expected' => [],
+		],
+
+		'shouldDieWithErrorWhenSettingsNotFound' => [
+			'config'   => [
+				'option_page'     => 'imagify',
+				'user_can'        => true,
+				'nonce_check'     => true,
+				'missing_options' => true,
+				'options'         => []
+			],
+			'expected' => [
+				'die_message' => '<strong>ERROR</strong>: options page not found.',
+			],
+		],
 
 		'shouldUpdateOptions' => [
 			'config'   => [
