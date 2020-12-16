25dd800d2bac22db79356d03822322cfbf019698
Caspar Green <cg@caspar.green>
2020-12-16 14:23:11 -0500

    Check for php8 CurlHandle object

--- inc/classes/class-imagify.php
+++ inc/classes/class-imagify.php
@@ -470,7 +470,7 @@ class Imagify {
 			$url = self::API_ENDPOINT . $url;
 			$ch  = @curl_init();
 
-			if ( ! is_resource( $ch ) ) {
+			if ( ! is_resource( $ch ) || 'CurlHandle' !== gettype( $ch ) ) {
 				throw new Exception( 'Could not initialize a new cURL handle' );
 			}
 
