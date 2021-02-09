77bef10f06f5735d70ab129fb7aa028e670b7b32
Caspar Green <cg@caspar.green>
2021-02-09 09:32:56 -0500

    Update version in Plugin Header

--- imagify.php
+++ imagify.php
@@ -3,7 +3,7 @@
  * Plugin Name: Imagify
  * Plugin URI: https://wordpress.org/plugins/imagify/
  * Description: Dramatically reduce image file sizes without losing quality, make your website load faster, boost your SEO and save money on your bandwidth using Imagify, the new most advanced image optimization tool.
- * Version: 1.9.13
+ * Version: 1.9.14
  * Requires at least: 4.0.0
  * Requires PHP: 5.4
  * Author: WP Media
