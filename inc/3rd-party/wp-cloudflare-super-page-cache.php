f4bad2b6c932dfffa0b17a7fbeedf0a1dc107cc6
Joe DiSalvo <joedisalvo04@gmail.com>
2022-01-05 11:48:17 -0500

    Making sure the WP Cloudflare Super Page Cache Sweet Alert is removed for all Imagify admin pages

--- inc/3rd-party/wp-cloudflare-super-page-cache.php
+++ inc/3rd-party/wp-cloudflare-super-page-cache.php
@@ -2,15 +2,24 @@
 defined( 'ABSPATH' ) || die( 'Cheatin uh?' );
 
 add_action( 'admin_enqueue_scripts', function( $hook_suffix ) {
+
+	$imagify_admin_pages = array(
+		'media_page_imagify-bulk-optimization',
+		'settings_page_imagify',
+		'media_page_imagify-files',
+		'nextgen-gallery_page_imagify-ngg-bulk-optimization'
+	);
+
 	if (
 		! is_admin()
 		||
-		'media_page_imagify-bulk-optimization' !== $hook_suffix
-		||
 		! class_exists( 'SWCFPC_Backend' )
+		||
+		! in_array( $hook_suffix , $imagify_admin_pages, true )
 	) {
 		return;
 	}
 
 	wp_deregister_script( 'swcfpc_sweetalert_js' );
+
 }, 100 );
