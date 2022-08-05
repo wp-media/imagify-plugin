ab41664dfd070234b857815f524c86efd9859ad9
Rémy Perona <remperona@gmail.com>
2022-08-05 16:28:30 -0400

    add command interface and abstract class

---
+++ classes/CLI/AbstractCommand.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Imagify\CLI;
+
+abstract class AbstractCommand implements CommandInterface {
+	/**
+     * {@inheritdoc}
+     */
+	final public function get_name() {
+		return sprintf( 'imagify %s', $this->get_command_name() );
+	}
+
+	/**
+     * Get the "imagify" command name.
+     *
+     * @return string
+     */
+	abstract protected function get_command_name();
+
+	/**
+     * {@inheritdoc}
+     */
+	public function get_synopsis() {
+		return [];
+	}
+}
