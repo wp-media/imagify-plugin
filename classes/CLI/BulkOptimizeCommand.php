87d367419e430bae2684c27ab56c26719442ea19
Rémy Perona <remperona@gmail.com>
2022-08-05 16:28:37 -0400

    add bulk optimize command class

---
+++ classes/CLI/BulkOptimizeCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Imagify\CLI;
+
+use Imagify\Bulk\Bulk;
+
+class BulkOptimizeCommand extends AbstractCommand {
+	/**
+     * {@inheritdoc}
+     */
+	public function __invoke( $arguments, $options ) {
+		Bulk::get_instance()->run_optimize( $arguments );
+	}
+
+	/**
+     * {@inheritdoc}
+     */
+	protected function get_command_name() {
+		return 'bulk-optimize';
+	}
+
+	/**
+     * {@inheritdoc}
+     */
+	public function get_description() {
+		return 'Run the bulk optimization';
+	}
+
+	/**
+     * {@inheritdoc}
+     */
+	public function get_synopsis() {
+		return [
+			[
+				'type'        => 'positional',
+				'name'        => 'contexts',
+				'description' => 'The context(s) to run the bulk optimization for',
+				'optional'    => false,
+				'repeating'   => true,
+			],
+		];
+	}
+}
