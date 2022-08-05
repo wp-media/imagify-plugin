b892254ad3e8585a0d539dad7333d0bfcc6bd1b4
Rémy Perona <remperona@gmail.com>
2022-08-05 16:34:11 -0400

    fix alignment

--- classes/CLI/AbstractCommand.php
+++ classes/CLI/AbstractCommand.php
@@ -4,22 +4,22 @@ namespace Imagify\CLI;
 
 abstract class AbstractCommand implements CommandInterface {
 	/**
-     * {@inheritdoc}
-     */
+	 * {@inheritdoc}
+	 */
 	final public function get_name() {
 		return sprintf( 'imagify %s', $this->get_command_name() );
 	}
 
 	/**
-     * Get the "imagify" command name.
-     *
-     * @return string
-     */
+	 * Get the "imagify" command name.
+	 *
+	 * @return string
+	 */
 	abstract protected function get_command_name();
 
 	/**
-     * {@inheritdoc}
-     */
+	 * {@inheritdoc}
+	 */
 	public function get_synopsis() {
 		return [];
 	}
--- classes/CLI/CommandInterface.php
+++ classes/CLI/CommandInterface.php
@@ -4,31 +4,31 @@ namespace Imagify\CLI;
 
 interface CommandInterface {
 	/**
-     * Get the command name.
-     *
-     * @return string
-     */
+	 * Get the command name.
+	 *
+	 * @return string
+	 */
 	public function get_name();
 
 	 /**
-     * Executes the command.
-     *
-     * @param array $arguments Positional argument.
-     * @param array $options Optional arguments.
-     */
+	 * Executes the command.
+	 *
+	 * @param array $arguments Positional argument.
+	 * @param array $options Optional arguments.
+	 */
 	public function __invoke( $arguments, $options );
 
 	/**
-     * Get the positional and associative arguments a command accepts.
-     *
-     * @return array
-     */
+	 * Get the positional and associative arguments a command accepts.
+	 *
+	 * @return array
+	 */
 	public function get_synopsis();
 
 	/**
-     * Get the command description.
-     *
-     * @return string
-     */
+	 * Get the command description.
+	 *
+	 * @return string
+	 */
 	public function get_description();
 }
