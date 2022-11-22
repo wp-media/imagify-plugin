a421021ecb26c7d360ae3c1c05756c0d15cc141b
Rémy Perona <remperona@gmail.com>
2022-11-22 15:15:30 -0500

    reduce transients time to 10 minutes

--- classes/Bulk/Bulk.php
+++ classes/Bulk/Bulk.php
@@ -87,7 +87,7 @@ class Bulk {
 			if ( false !== strpos( $item['process_class'], 'CustomFolders' ) ) {
 				$custom_folders['remaining']--;
 
-				set_transient( 'imagify_custom-folders_optimize_running', $custom_folders, DAY_IN_SECONDS );
+				set_transient( 'imagify_custom-folders_optimize_running', $custom_folders, 10 * MINUTE_IN_SECONDS );
 
 				$remaining += $custom_folders['remaining'];
 			}
@@ -97,7 +97,7 @@ class Bulk {
 			if ( false !== strpos( $item['process_class'], 'WP' ) ) {
 				$library_wp['remaining']--;
 
-				set_transient( 'imagify_wp_optimize_running', $library_wp, DAY_IN_SECONDS );
+				set_transient( 'imagify_wp_optimize_running', $library_wp, 10 * MINUTE_IN_SECONDS );
 
 				$remaining += $library_wp['remaining'];
 			}
@@ -127,7 +127,7 @@ class Bulk {
 		$counter['total']     = $counter['total'] - 1;
 		$counter['remaining'] = $counter['remaining'] - 1;
 
-		set_transient( "imagify_{$context}_optimize_running", $counter, DAY_IN_SECONDS );
+		set_transient( "imagify_{$context}_optimize_running", $counter, 10 * MINUTE_IN_SECONDS );
 	}
 
 	/**
@@ -197,7 +197,7 @@ class Bulk {
 			'remaining' => count( $media_ids ),
 		];
 
-		set_transient( "imagify_{$context}_optimize_running", $data, DAY_IN_SECONDS );
+		set_transient( "imagify_{$context}_optimize_running", $data, 10 * MINUTE_IN_SECONDS );
 
 		return [
 			'success' => true,
