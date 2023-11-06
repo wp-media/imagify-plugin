e856e261d23cf36b0bc42e33f02b417b7b60276c
Rémy Perona <remperona@gmail.com>
2023-11-06 16:48:07 -0500

    update view content

--- classes/Admin/AdminBar.php
+++ classes/Admin/AdminBar.php
@@ -35,6 +35,28 @@ class AdminBar {
 		$user             = new User();
 		$views            = Imagify_Views::get_instance();
 		$unconsumed_quota = $views->get_quota_percent();
+		$text             = '';
+		$button_text      = '';
+		$upgrade_link     = '';
+
+		if ( $user->is_free() ) {
+			$text         = esc_html__( 'Upgrade your plan now for more!', 'rocket' ) . '<br>' .
+			esc_html__( 'From $4.99/month only, keep going with image optimization!', 'rocket' );
+			$button_text  = esc_html__( 'Upgrade My Plan', 'rocket' );
+			$upgrade_link = IMAGIFY_APP_DOMAIN . '/subscription/?utm_source=plugin&utm_medium=notification';
+		} elseif ( $user->is_growth() ) {
+			$text = esc_html__( 'Switch to Infinite plan for unlimited optimization:', 'rocket' )  . '<br>';
+
+			if ( $user->is_monthly ) {
+				$text         .= esc_html__( 'For $9.99/month, optimize as many images as you like!', 'rocket' );
+				$upgrade_link = IMAGIFY_APP_DOMAIN . '/subscription/plan_switch/?label=infinite&payment_plan=1&utm_source=plugin&utm_medium=notification ';
+			} else {
+				$text         .= esc_html__( 'For $99.9/year, optimize as many images as you like!', 'rocket' );
+				$upgrade_link = IMAGIFY_APP_DOMAIN . '/subscription/plan_switch/?label=infinite&payment_plan=2&utm_source=plugin&utm_medium=notification ';
+			}
+
+			$button_text = esc_html__( 'Switch To Infinite Plan', 'rocket' );
+		}
 
 		$data = [
 			'quota_icon'       => $views->get_quota_icon(),
@@ -44,6 +66,9 @@ class AdminBar {
 			'unconsumed_quota' => $unconsumed_quota,
 			'user_quota'       => $user->quota,
 			'next_update'      => $user->next_date_update,
+			'text'             => $text,
+			'button_text'      => $button_text,
+			'upgrade_link'     => $upgrade_link,
 		];
 
 		$template = $views->get_template( 'admin/admin-bar-status', $data );
