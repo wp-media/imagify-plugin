a03a7bd9ba3827ba8c328125f931eff90d320f1f
Opeyemi Ibrahim <opeyemi.khadri@gmail.com>
2024-01-16 16:43:04 +0100

    Add dependencies classes close #776

---
+++ classes/Dependencies/League/Container/Exception/ContainerException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Imagify\Dependencies\League\Container\Exception;
+
+use Imagify\Dependencies\Psr\Container\ContainerExceptionInterface;
+use RuntimeException;
+
+class ContainerException extends RuntimeException implements ContainerExceptionInterface
+{
+}
