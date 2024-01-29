b396de27cda71d9b16e311a0f55f482bebf13380
Rémy Perona <remy@wp-media.me>
2024-01-29 12:07:15 -0500

    Update WP background processing and Action Scheduler (#786)

--- inc/classes/Dependencies/deliciousbrains/wp-background-processing/classes/wp-async-request.php
+++ inc/classes/Dependencies/deliciousbrains/wp-background-processing/classes/wp-async-request.php
@@ -5,6 +5,11 @@
  * @package WP-Background-Processing
  */
 
+// phpcs:disable Generic.Commenting.DocComment.MissingShort
+/** @noinspection PhpIllegalPsrClassPathInspection */
+/** @noinspection AutoloadingIssuesInspection */
+// phpcs:disable Generic.Commenting.DocComment.MissingShort
+
 /**
  * Abstract Imagify_WP_Async_Request class.
  *
@@ -51,7 +56,7 @@ abstract class Imagify_WP_Async_Request {
 	protected $data = array();
 
 	/**
-	 * Initiate new async request
+	 * Initiate new async request.
 	 */
 	public function __construct() {
 		$this->identifier = $this->prefix . '_' . $this->action;
@@ -61,7 +66,7 @@ abstract class Imagify_WP_Async_Request {
 	}
 
 	/**
-	 * Set data used during the request
+	 * Set data used during the request.
 	 *
 	 * @param array $data Data.
 	 *
@@ -74,9 +79,9 @@ abstract class Imagify_WP_Async_Request {
 	}
 
 	/**
-	 * Dispatch the async request
+	 * Dispatch the async request.
 	 *
-	 * @return array|WP_Error
+	 * @return array|WP_Error|false HTTP Response array, WP_Error on failure, or false if not attempted.
 	 */
 	public function dispatch() {
 		$url  = add_query_arg( $this->get_query_args(), $this->get_query_url() );
@@ -86,7 +91,7 @@ abstract class Imagify_WP_Async_Request {
 	}
 
 	/**
-	 * Get query args
+	 * Get query args.
 	 *
 	 * @return array
 	 */
@@ -109,7 +114,7 @@ abstract class Imagify_WP_Async_Request {
 	}
 
 	/**
-	 * Get query URL
+	 * Get query URL.
 	 *
 	 * @return string
 	 */
@@ -129,7 +134,7 @@ abstract class Imagify_WP_Async_Request {
 	}
 
 	/**
-	 * Get post args
+	 * Get post args.
 	 *
 	 * @return array
 	 */
@@ -139,11 +144,11 @@ abstract class Imagify_WP_Async_Request {
 		}
 
 		$args = array(
-			'timeout'   => 0.01,
+			'timeout'   => 5,
 			'blocking'  => false,
 			'body'      => $this->data,
-			'cookies'   => $_COOKIE,
-			'sslverify' => apply_filters( 'https_local_ssl_verify', false ),
+			'cookies'   => $_COOKIE, // Passing cookies ensures request is performed as initiating user.
+			'sslverify' => apply_filters( 'https_local_ssl_verify', false ), // Local requests, fine to pass false.
 		);
 
 		/**
@@ -155,27 +160,49 @@ abstract class Imagify_WP_Async_Request {
 	}
 
 	/**
-	 * Maybe handle
+	 * Maybe handle a dispatched request.
 	 *
 	 * Check for correct nonce and pass to handler.
+	 *
+	 * @return void|mixed
 	 */
 	public function maybe_handle() {
-		// Don't lock up other requests while processing
+		// Don't lock up other requests while processing.
 		session_write_close();
 
 		check_ajax_referer( $this->identifier, 'nonce' );
 
 		$this->handle();
 
-		wp_die();
+		return $this->maybe_wp_die();
 	}
 
 	/**
-	 * Handle
+	 * Should the process exit with wp_die?
+	 *
+	 * @param mixed $return What to return if filter says don't die, default is null.
+	 *
+	 * @return void|mixed
+	 * @noinspection ForgottenDebugOutputInspection
+	 */
+	protected function maybe_wp_die( $return = null ) {
+		/**
+		 * Should wp_die be used?
+		 *
+		 * @return bool
+		 */
+		if ( apply_filters( $this->identifier . '_wp_die', true ) ) {
+			wp_die();
+		}
+
+		return $return;
+	}
+
+	/**
+	 * Handle a dispatched request.
 	 *
 	 * Override this method to perform any actions required
 	 * during the async request.
 	 */
 	abstract protected function handle();
-
 }
