952db3f8bff94462ebf14ab2d069a4e0d80da02b
Rémy Perona <remperona@gmail.com>
2024-02-01 18:04:01 -0500

    add service provider

---
+++ classes/Webp/ServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace Imagify\Webp;
+
+use Imagify\Dependencies\League\Container\ServiceProvider\AbstractServiceProvider;
+use Imagify\Webp\RewriteRules\Display as RewriteRules;
+
+class ServiceProvider extends AbstractServiceProvider {
+	/**
+	 * Services provided by this provider
+	 *
+	 * @var array
+	 */
+	protected $provides = [
+		'webp_display',
+		'webp_rewrite_rules',
+	];
+
+	/**
+	 * Subscribers provided by this provider
+	 *
+	 * @var array
+	 */
+	public $subscribers = [
+		'webp_display',
+		'webp_rewrite_rules',
+	];
+
+	/**
+	 * Registers the provided classes
+	 *
+	 * @return void
+	 */
+	public function register() {
+		$this->getContainer()->share( 'webp_display', Display::class );
+		$this->getContainer()->share( 'webp_rewrite_rules', RewriteRules::class );
+	}
+}
