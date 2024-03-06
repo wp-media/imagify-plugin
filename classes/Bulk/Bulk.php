1ee7d5ad4f38309d04e50f6a4c17740b149d2429
Rémy Perona <remperona@gmail.com>
2024-03-06 11:29:41 -0500

    fix number of media in the bulk optimization

--- classes/Bulk/Bulk.php
+++ classes/Bulk/Bulk.php
@@ -173,21 +173,8 @@ class Bulk {
 				'message' => 'over-quota',
 			];
 		}
-		$formats = imagify_nextgen_images_formats();
-		$media_ids = [
-			'ids' => [],
-			'errors' => [
-				'no_file_path' => [],
-				'no_backup' => [],
-			],
-		];
-		foreach ( $formats as $format ) {
-			$result = $this->get_bulk_instance( $context )->get_optimized_media_ids_without_format( $format );
-			$media_ids['ids'] = array_merge( $media_ids['ids'], $result['ids'] );
-		}
-		$get_unoptimized_media_ids = $this->get_bulk_instance( $context )->get_unoptimized_media_ids( $optimization_level );
 
-		$media_ids['ids'] = array_merge( $media_ids['ids'], $get_unoptimized_media_ids );
+		$media_ids = $this->get_bulk_instance( $context )->get_unoptimized_media_ids( $optimization_level );
 
 		if ( empty( $media_ids['ids'] ) ) {
 			return [
