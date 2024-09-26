820f6a7512662bcef93c8d71dea9a5a1bf40945b
Rémy Perona <remperona@gmail.com>
2024-09-26 11:45:55 -0400

    add service provider for admin

---
+++ classes/Admin/ServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types=1);
+
+namespace Imagify\Admin;
+
+use Imagify\Dependencies\League\Container\ServiceProvider\AbstractServiceProvider;
+
+/**
+ * Service provider for Admin
+ */
+class ServiceProvider extends AbstractServiceProvider {
+	/**
+	 * Services provided by this provider
+	 *
+	 * @var array
+	 */
+	protected $provides = [
+		'admin_bar',
+	];
+
+	/**
+	 * Subscribers provided by this provider
+	 *
+	 * @var array
+	 */
+	public $subscribers = [
+		'admin_bar',
+	];
+
+	/**
+	 * Registers the provided classes
+	 *
+	 * @return void
+	 */
+	public function register() {
+		$this->getContainer()->share( 'admin_bar', AdminBar::class );
+	}
+
+	/**
+	 * Returns the subscribers array
+	 *
+	 * @return array
+	 */
+	public function get_subscribers() {
+		return $this->subscribers;
+	}
+}
