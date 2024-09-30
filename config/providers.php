f5d85e24c26513967907f50e25dd0f59a27ed467
Rémy Perona <remperona@gmail.com>
2024-09-30 15:22:47 -0400

    update loading order

--- config/providers.php
+++ config/providers.php
@@ -1,10 +1,10 @@
 <?php
 return [
+	'Imagify\User\ServiceProvider',
 	'Imagify\Admin\ServiceProvider',
 	'Imagify\Avif\ServiceProvider',
 	'Imagify\CDN\ServiceProvider',
 	'Imagify\Picture\ServiceProvider',
 	'Imagify\Stats\ServiceProvider',
 	'Imagify\Webp\ServiceProvider',
-	'Imagify\User\ServiceProvider',
 ];
