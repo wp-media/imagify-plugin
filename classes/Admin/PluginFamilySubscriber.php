fe368c138d3e2743b152b7baba57e4260868841b
Michael Lee <michaelleemichaellee408@gmail.com>
2024-10-21 11:18:45 +0100

    Added new subscriber for plugin family

--- classes/Admin/PluginFamilySubscriber.php
+++ classes/Admin/PluginFamilySubscriber.php
@@ -4,10 +4,45 @@ declare(strict_types=1);
 namespace Imagify\Admin;
 
 use Imagify\EventManagement\SubscriberInterface;
+use WPMedia\PluginFamily\Controller\{ PluginFamily, PluginFamilyInterface };
 
 /**
  * Process plugin family actions
  */
-class PluginFamilySubscriber implements SubscriberInterface {
+class PluginFamilySubscriber implements SubscriberInterface, PluginFamilyInterface {
 
+    /**
+     * PluginFamily instance.
+     *
+     * @var PluginFamily
+     */
+    protected $plugin_family;
+
+    /**
+	 * Instantiate the class
+	 *
+	 * @param PluginFamily $plugin_family PluginFamily instance.
+	 */
+    public function __construct( PluginFamily $plugin_family ) {
+        $this->plugin_family = $plugin_family;
+    }
+
+    /**
+	 * Returns an array of events this subscriber listens to
+	 *
+	 * @return array
+	 */
+	public static function get_subscribed_events() {
+        $events = PluginFamily::get_subscribed_events();
+
+        return $events;
+	}
+
+    public function install_activate() {
+        $this->plugin_family->install_activate();
+    }
+
+    public function display_error_notice() {
+        $this->plugin_family->display_error_notice();
+    }
 }
