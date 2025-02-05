36b1e3ce3bda496b4d7a9e794b11c948958d1324
Rémy Perona <remperona@gmail.com>
2025-02-05 15:53:23 -0500

    fix alignment

--- views/admin/admin-bar-status.php
+++ views/admin/admin-bar-status.php
@@ -21,8 +21,10 @@ $plan_label = false !== $pos ? substr( $data['plan_label'], 0, $pos ) : $data['p
 			<?php
 				printf(
 				// translators: %s = percentage.
-				__( 'You have %s space credit left', 'imagify' ), '<span class="imagify-unconsumed-percent">' . $data['unconsumed_quota'] . '%</span>' );
-				?></p>
+					__( 'You have %s space credit left', 'imagify' ),
+					'<span class="imagify-unconsumed-percent">' . $data['unconsumed_quota'] . '%</span>'
+				);
+			?></p>
 			<div class="<?php echo esc_attr( $data['quota_class'] ); ?>">
 				<div style="width: <?php echo esc_attr( $data['unconsumed_quota'] ); ?>%;" class="imagify-unconsumed-bar imagify-progress"></div>
 			</div>
