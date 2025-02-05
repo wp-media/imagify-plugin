205a5782cb54938b25646f9d7bb85abb99671507
Rémy Perona <remperona@gmail.com>
2025-02-05 15:38:58 -0500

    fix alignment

--- views/notice-grid-view.php
+++ views/notice-grid-view.php
@@ -1,13 +1,19 @@
 <?php
-defined( 'ABSPATH' ) || die( 'Cheatin’ uh?' );
+defined( 'ABSPATH' ) || exit;
 
-$this->print_template( 'notice-header', array(
-	'title' => __( 'You\'re missing out!', 'imagify' ),
-) );
+$this->print_template(
+	'notice-header',
+	[
+		'title' => __( 'You\'re missing out!', 'imagify' ),
+	]
+);
 ?>
 <p><?php _e( 'Use the List view to optimize images with Imagify.', 'imagify' ); ?></p>
 <p><a href="<?php echo esc_url( admin_url( 'upload.php?mode=list' ) ); ?>"><?php _e( 'Switch to the List View', 'imagify' ); ?></a></p>
 <?php
-$this->print_template( 'notice-footer', array(
-	'dismissible' => 'grid-view',
-) );
+$this->print_template(
+	'notice-footer',
+	[
+		'dismissible' => 'grid-view',
+	]
+);
