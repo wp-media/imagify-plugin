f30136cddf128920774aaf8e518157d84ef510a9
Rémy Perona <remperona@gmail.com>
2025-02-05 15:05:02 -0500

    fix alignment

--- classes/Optimization/Process/AbstractProcess.php
+++ classes/Optimization/Process/AbstractProcess.php
@@ -1628,13 +1628,22 @@ abstract class AbstractProcess implements ProcessInterface {
 		}
 
 		$keys = array_keys( $sizes );
-		$non_next_gen_keys = array_values(array_filter($keys, function ( $key ) {
-			return strpos( (string) $key, $this->format ) === false;
-		}));
 
-		return array_reduce($non_next_gen_keys, function ( $is_fully, $key ) use ( $sizes ) {
-			return key_exists( $key . $this->format, $sizes ) && $is_fully;
-		}, true);
+		$non_next_gen_keys = array_values(
+			array_filter(
+				$keys, function ( $key ) {
+					return strpos( (string) $key, $this->format ) === false;
+				}
+			)
+		);
+
+		return array_reduce(
+			$non_next_gen_keys,
+			function ( $is_fully, $key ) use ( $sizes ) {
+				return key_exists( $key . $this->format, $sizes ) && $is_fully;
+			},
+			true
+		);
 	}
 
 	/**
