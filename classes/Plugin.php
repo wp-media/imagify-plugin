29f87ba0b379c6a265996a75d1d4200ce86b48d3
Rémy Perona <remperona@gmail.com>
2025-02-19 14:41:11 -0500

    remove direct calls to translations function

--- classes/Plugin.php
+++ classes/Plugin.php
@@ -131,9 +131,6 @@ class Plugin {
 
 		add_action( 'init', [ $this, 'maybe_activate' ] );
 
-		// Load plugin translations.
-		imagify_load_translations();
-
 		imagify_add_command( new BulkOptimizeCommand() );
 		imagify_add_command( new GenerateMissingNextgenCommand() );
 
