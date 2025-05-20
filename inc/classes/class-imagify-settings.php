f87afb9ebee37c894bb88d0575c2cc3e8ce0c7bd
Rémy Perona <remperona@gmail.com>
2025-05-20 15:52:38 -0400

    remove extra spacing

--- inc/classes/class-imagify-settings.php
+++ inc/classes/class-imagify-settings.php
@@ -491,7 +491,7 @@ class Imagify_Settings {
 		$attributes         = array_merge( $attributes, $args['attributes'] );
 		$args['attributes'] = self::build_attributes( $attributes );
 		?>
-		<input type="checkbox" value="1" 
+		<input type="checkbox" value="1"
 		<?php
 		checked( $current_value, 1 );
 		?>
@@ -500,29 +500,17 @@ class Imagify_Settings {
 		?>
 />
 		<!-- Empty onclick attribute to make clickable labels on iTruc & Mac -->
-		<label for="
-		<?php
-		echo $attributes['id'];
-		?>
-		" onclick="">
-		<?php
-			echo $args['label'];
-		?>
+		<label for="<?php echo $attributes['id']; ?>" onclick="">
+		<?php echo $args['label']; ?>
 </label>
 		<?php
 		if ( ! $args['info'] ) {
 			return;
 		}
 		?>
-		<span id="
-		<?php
-		echo $attributes['aria-describedby'];
-		?>
-		" class="imagify-info">
+		<span id="<?php echo $attributes['aria-describedby']; ?>" class="imagify-info">
 			<span class="dashicons dashicons-info"></span>
-			<?php
-			echo $args['info'];
-			?>
+			<?php echo $args['info']; ?>
 		</span>
 		<?php
 	}
@@ -587,18 +575,12 @@ class Imagify_Settings {
 		$display_check_all = $nb_of_values > 3;
 		$nb_of_checked     = 0;
 		?>
-		<fieldset class="imagify-check-group
-		<?php
-		echo $nb_of_values > 5 ? ' imagify-is-scrollable' : '';
-		?>
-		">
+		<fieldset class="imagify-check-group <?php echo $nb_of_values > 5 ? ' imagify-is-scrollable' : ''; ?>">
 			<?php
 			if ( $args['legend'] ) {
 				?>
 				<legend class="screen-reader-text">
-				<?php
-					echo $args['legend'];
-				?>
+				<?php echo $args['legend']; ?>
 				</legend>
 				<?php
 			}
@@ -620,34 +602,10 @@ class Imagify_Settings {
 				}
 				?>
 				<p>
-					<input type="checkbox" value="
-					<?php
-					echo esc_attr( $value );
-					?>
-					" id="
-					<?php
-					echo $input_id;
-					?>
-"
-				<?php
-					echo $args['attributes'];
-				?>
-				<?php
-					checked( $checked );
-				?>
-				<?php
-					disabled( $disabled );
-				?>
-/>
-					<label for="
-					<?php
-					echo $input_id;
-					?>
-					" onclick="">
-					<?php
-						echo $label;
-					?>
-</label>
+					<input type="checkbox" value="<?php echo esc_attr( $value ); ?>" id="<?php echo $input_id; ?>" <?php echo $args['attributes']; ?> <?php checked( $checked ); ?> <?php disabled( $disabled ); ?> />
+					<label for="<?php echo $input_id; ?>" onclick="">
+					<?php echo $label; ?>
+					</label>
 				</p>
 				<?php
 			}
@@ -662,27 +620,19 @@ class Imagify_Settings {
 			}
 			?>
 			<p class="hide-if-no-js imagify-select-all-buttons">
-				<button type="button" class="imagify-link-like imagify-select-all
-				<?php
-				echo $all_checked ? ' imagify-is-inactive" aria-disabled="true' : '';
-				?>
-				" data-action="select">
+				<button type="button" class="imagify-link-like imagify-select-all <?php echo $all_checked ? ' imagify-is-inactive" aria-disabled="true' : ''; ?>" data-action="select">
 				<?php
 					_e( 'Select All', 'imagify' );
 				?>
-</button>
+				</button>
 
 				<span class="imagify-pipe"></span>
 
-				<button type="button" class="imagify-link-like imagify-select-all
-				<?php
-				echo $nb_of_checked ? '' : ' imagify-is-inactive" aria-disabled="true';
-				?>
-				" data-action="unselect">
+				<button type="button" class="imagify-link-like imagify-select-all <?php echo $nb_of_checked ? '' : ' imagify-is-inactive" aria-disabled="true'; ?>	" data-action="unselect">
 				<?php
 					_e( 'Unselect All', 'imagify' );
 				?>
-</button>
+				</button>
 			</p>
 			<?php
 		}
@@ -759,31 +709,10 @@ class Imagify_Settings {
 			foreach ( $args['values'] as $value => $label ) {
 				$input_id = sprintf( $id_attribute, sanitize_html_class( $value ) );
 				?>
-				<input type="radio" value="
-				<?php
-				echo esc_attr( $value );
-				?>
-				" id="
-				<?php
-				echo $input_id;
-				?>
-"
-				<?php
-				echo $args['attributes'];
-				?>
-				<?php
-				checked( $current_value, $value );
-				?>
-/>
-				<label for="
-				<?php
-				echo $input_id;
-				?>
-				" onclick="">
-				<?php
-					echo $label;
-				?>
-</label>
+				<input type="radio" value="<?php echo esc_attr( $value ); ?>" id="<?php echo $input_id; ?>" <?php echo $args['attributes']; ?> <?php checked( $current_value, $value ); ?> />
+				<label for="<?php echo $input_id; ?>" onclick="">
+				<?php echo $label; ?>
+				</label>
 				<br/>
 				<?php
 			}
@@ -794,15 +723,9 @@ class Imagify_Settings {
 			return;
 		}
 		?>
-		<span id="
-		<?php
-		echo $attributes['aria-describedby'];
-		?>
-		" class="imagify-info">
+		<span id="<?php echo $attributes['aria-describedby']; ?>" class="imagify-info">
 			<span class="dashicons dashicons-info"></span>
-			<?php
-			echo $args['info'];
-			?>
+			<?php echo $args['info']; ?>
 		</span>
 		<?php
 	}
@@ -866,15 +789,9 @@ class Imagify_Settings {
 			}
 			?>
 			</p>
-			<span id="
-			<?php
-			echo $attributes['aria-describedby'];
-			?>
-			" class="imagify-<?php echo esc_attr( $args['info_class'] ); ?>">
+			<span id="<?php echo $attributes['aria-describedby']; ?>" class="imagify-<?php echo esc_attr( $args['info_class'] ); ?>">
 				<span class="dashicons dashicons-info"></span>
-				<?php
-				echo $args['info'];
-				?>
+				<?php echo $args['info']; ?>
 			</span>
 		</div>
 		<?php
@@ -931,38 +848,18 @@ class Imagify_Settings {
 		$args['attributes'] = self::build_attributes( $attributes );
 		?>
 		<!-- Empty onclick attribute to make clickable labels on iTruc & Mac -->
-		<label for="
-		<?php
-		echo $attributes['id'];
-		?>
-		" onclick="">
-		<?php
-			echo $args['label'];
-		?>
-</label>
-		<input type="text" value="
-		<?php
-		echo esc_attr( $current_value );
-		?>
-		"
-		<?php
-		echo $args['attributes'];
-		?>
-/>
+		<label for="<?php echo $attributes['id']; ?>" onclick="">
+		<?php echo $args['label']; ?>
+		</label>
+		<input type="text" value="<?php echo esc_attr( $current_value ); ?>" <?php echo $args['attributes']; ?> />
 		<?php
 		if ( ! $args['info'] ) {
 			return;
 		}
 		?>
-		<span id="
-		<?php
-		echo $attributes['aria-describedby'];
-		?>
-		" class="imagify-info">
+		<span id="<?php echo $attributes['aria-describedby']; ?>" class="imagify-info">
 			<span class="dashicons dashicons-info"></span>
-			<?php
-			echo $args['info'];
-			?>
+			<?php echo $args['info']; ?>
 		</span>
 		<?php
 	}
@@ -1009,15 +906,7 @@ class Imagify_Settings {
 		$attributes         = array_merge( $attributes, $args['attributes'] );
 		$args['attributes'] = self::build_attributes( $attributes );
 		?>
-		<input type="hidden" value="
-		<?php
-		echo esc_attr( $current_value );
-		?>
-		"
-		<?php
-		echo $args['attributes'];
-		?>
-/>
+		<input type="hidden" value="<?php echo esc_attr( $current_value ); ?>" <?php echo $args['attributes']; ?> />
 		<?php
 	}
 
