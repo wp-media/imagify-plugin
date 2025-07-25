4f6b2b3535b9d2e66fb6eac58b17204ecb3b4f3c
Rémy Perona <remperona@gmail.com>
2025-07-25 16:25:21 -0400

    add escapes

--- views/notice-footer.php
+++ views/notice-footer.php
@@ -3,6 +3,6 @@ defined( 'ABSPATH' ) || die( 'Cheatin’ uh?' );
 ?>
 	</div>
 	<?php if ( ! empty( $data['dismissible'] ) ) : ?>
-		<a href="<?php echo esc_url( get_imagify_admin_url( 'dismiss-notice', $data['dismissible'] ) ); ?>" class="imagify-notice-dismiss notice-dismiss" title="<?php esc_attr_e( 'Dismiss this notice', 'imagify' ); ?>"><span class="screen-reader-text"><?php _e( 'Dismiss this notice', 'imagify' ); ?></span></a>
+		<a href="<?php echo esc_url( get_imagify_admin_url( 'dismiss-notice', $data['dismissible'] ) ); ?>" class="imagify-notice-dismiss notice-dismiss" title="<?php esc_attr_e( 'Dismiss this notice', 'imagify' ); ?>"><span class="screen-reader-text"><?php esc_html_e( 'Dismiss this notice', 'imagify' ); ?></span></a>
 	<?php endif; ?>
 </div>
