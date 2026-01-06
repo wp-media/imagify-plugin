28af86615b2539e7f5cad44d316ef50794230a62
Rémy Perona <remperona@gmail.com>
2026-01-06 14:29:02 -0500

    update param

--- inc/functions/partners.php
+++ inc/functions/partners.php
@@ -4,8 +4,7 @@ defined( 'ABSPATH' ) || die( 'Cheatin’ uh?' );
 /**
  * Get the partner ID stored in the database.
  *
- * @since  1.6.14
- * @author Grégory Viguier
+ * @since 1.6.14
  *
  * @return string|bool The partner ID. False otherwise.
  */
@@ -26,8 +25,7 @@ function imagify_get_partner() {
 /**
  * Delete the partner ID stored in the database.
  *
- * @since  1.6.14
- * @author Grégory Viguier
+ * @since 1.6.14
  */
 function imagify_delete_partner() {
 	if ( class_exists( 'Imagify_Partner' ) ) {
@@ -40,7 +38,9 @@ function imagify_delete_partner() {
 /**
  * Save the partner ID to hide our other plugins if needed
  *
- * @param int $partner Partner ID.
+ * @since 2.2.7
+ *
+ * @param string|bool $partner Partner ID.
  *
  * @return void
  */
