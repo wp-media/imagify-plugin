635d30e2b7f0b24d3e96a1721628083753cd0f54
Rémy Perona <remperona@gmail.com>
2026-01-08 17:04:40 -0500

    debug test

--- Tests/Unit/inc/common/Partners/ImagifyMaybeDeletePartnerOnOptionUpdateTest.php
+++ Tests/Unit/inc/common/Partners/ImagifyMaybeDeletePartnerOnOptionUpdateTest.php
@@ -23,28 +23,6 @@ class ImagifyMaybeDeletePartnerOnOptionUpdateTest extends TestCase {
 	 * @runInSeparateProcess
 	 */
 	public function testShouldDoExpected( $old_value, $new_value, $partner, $expected ) {
-		if ( false === $expected ) {
-			Functions\expect( 'imagify_get_partner' )
-				->never();
 
-			Functions\expect( 'imagify_save_partner_hide_our_plugins' )
-				->never();
-
-			Functions\expect( 'imagify_delete_partner' )
-				->never();
-		} else {
-			Functions\expect( 'imagify_get_partner' )
-				->once()
-				->andReturn( $partner );
-
-			Functions\expect( 'imagify_save_partner_hide_our_plugins' )
-				->once()
-				->with( $partner );
-
-			Functions\expect( 'imagify_delete_partner' )
-				->once();
-		}
-
-		imagify_maybe_delete_partner_on_option_update( $old_value, $new_value );
 	}
 }
