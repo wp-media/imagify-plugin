68aabf693939bc0f7cb7810430044058d5240eef
Rémy Perona <remperona@gmail.com>
2026-01-08 16:47:51 -0500

    use direct option name

--- inc/common/partners.php
+++ inc/common/partners.php
@@ -1,7 +1,7 @@
 <?php
 defined( 'ABSPATH' ) || die( 'Cheatin’ uh?' );
 
-add_action( 'update_option_' . Imagify_Options::get_instance()->get_option_name(), 'imagify_maybe_delete_partner_on_option_update', 10, 2 );
+add_action( 'update_option_imagify_settings', 'imagify_maybe_delete_partner_on_option_update', 10, 2 );
 /**
  * After the first API key has been successfully added, make sure the partner ID is deleted.
  *
@@ -25,7 +25,7 @@ function imagify_maybe_delete_partner_on_option_update( $old_value, $new_value )
 	}
 }
 
-add_action( 'update_site_option_' . Imagify_Options::get_instance()->get_option_name(), 'imagify_maybe_delete_partner_on_network_option_update', 10, 3 );
+add_action( 'update_site_option_imagify_settings', 'imagify_maybe_delete_partner_on_network_option_update', 10, 3 );
 /**
  * After the first API key has been successfully added to the network option, make sure the partner ID is deleted.
  *
