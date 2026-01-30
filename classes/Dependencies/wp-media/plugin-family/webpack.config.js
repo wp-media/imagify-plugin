a3dbb63467644c3ddf61c0543fc69be81854305b
Rémy Perona <remperona@gmail.com>
2026-01-30 09:20:51 -0500

    remove dependencies from commit

--- classes/Dependencies/wp-media/plugin-family/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const defaultConfig = require('@wordpress/scripts/config/webpack.config');
-
-module.exports = {
-	...defaultConfig,
-	output: {
-		...defaultConfig.output,
-		path: __dirname + '/src/assets/js',
-	},
-};
